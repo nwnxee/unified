df54659fb94db1fbd67349fa06fda37ff850ceae
Bhaal <bhaal@marcaargentea.net>
2018-03-23 11:22:19 +0100

    Added compatibibility with GetEntireItemAppearance/RestoreItemAppearance of plugin version 2

--- Plugins/Item/Item.cpp
+++ Plugins/Item/Item.cpp
@@ -19,14 +19,14 @@ static ViewPtr<Item::Item> g_plugin;
 NWNX_PLUGIN_ENTRY Plugin::Info* PluginInfo()
 {
   return new Plugin::Info
-    {
-      "Item",
-	"Functions exposing additional item properties",
-	"Various / sherincall / Bhaal",
-	"marca.argentea at gmail.com",
-	3,
-	true
-    };
+  {
+    "Item",
+    "Functions exposing additional item properties",
+    "Various / sherincall / Bhaal",
+    "marca.argentea at gmail.com",
+    3,
+    true
+  };
 }
 
 NWNX_PLUGIN_ENTRY Plugin* PluginLoad(Plugin::CreateParams params)
@@ -41,7 +41,7 @@ namespace Item {
 Item::Item(const Plugin::CreateParams& params)
   : Plugin(params)
 {
-#define REGISTER(func)							\
+#define REGISTER(func)              \
   GetServices()->m_events->RegisterEvent(#func, std::bind(&Item::func, this, std::placeholders::_1))
 
   REGISTER(SetWeight);
@@ -66,18 +66,18 @@ CNWSItem *Item::item(ArgumentStack& args)
   const auto objectId = Services::Events::ExtractArgument<Types::ObjectID>(args);
 
   if (objectId == Constants::OBJECT_INVALID)
-    {
-      LOG_NOTICE("NWNX_Item function called on OBJECT_INVALID");
-      return nullptr;
-    }
+  {
+    LOG_NOTICE("NWNX_Item function called on OBJECT_INVALID");
+    return nullptr;
+  }
 
 
   auto *pGameObject = Globals::AppManager()->m_pServerExoApp->GetGameObject(objectId);
   if(pGameObject==nullptr || pGameObject->m_nObjectType!=Constants::OBJECT_TYPE_ITEM)
-    {
-      LOG_NOTICE("NWNX_Item function called on non item object");
-      return nullptr;
-    }
+  {
+    LOG_NOTICE("NWNX_Item function called on non item object");
+    return nullptr;
+  }
 
   return static_cast<CNWSItem*>(pGameObject);
 }
@@ -87,10 +87,10 @@ ArgumentStack Item::SetWeight(ArgumentStack&& args)
 {
   ArgumentStack stack;
   if (auto *pItem = item(args))
-    {
-      const auto w = Services::Events::ExtractArgument<int32_t>(args);
-      pItem->m_nWeight = w;
-    }
+  {
+    const auto w = Services::Events::ExtractArgument<int32_t>(args);
+    pItem->m_nWeight = w;
+  }
   return stack;
 }
 
@@ -98,10 +98,10 @@ ArgumentStack Item::SetBaseGoldPieceValue(ArgumentStack&& args)
 {
   ArgumentStack stack;
   if (auto *pItem = item(args))
-    {
-      const auto g = Services::Events::ExtractArgument<int32_t>(args);
-      pItem->m_nBaseUnitCost = g;
-    }
+  {
+    const auto g = Services::Events::ExtractArgument<int32_t>(args);
+    pItem->m_nBaseUnitCost = g;
+  }
   return stack;
 }
 
@@ -109,10 +109,10 @@ ArgumentStack Item::SetAddGoldPieceValue(ArgumentStack&& args)
 {
   ArgumentStack stack;
   if (auto *pItem = item(args))
-    {
-      const auto g = Services::Events::ExtractArgument<int32_t>(args);
-      pItem->m_nAdditionalCost = g;
-    }
+  {
+    const auto g = Services::Events::ExtractArgument<int32_t>(args);
+    pItem->m_nAdditionalCost = g;
+  }
   return stack;
 }
 
@@ -121,9 +121,9 @@ ArgumentStack Item::GetBaseGoldPieceValue(ArgumentStack&& args)
   ArgumentStack stack;
   int32_t retval = -1;
   if (auto *pItem = item(args))
-    {
-      retval = pItem->m_nBaseUnitCost;
-    }
+  {
+    retval = pItem->m_nBaseUnitCost;
+  }
   Services::Events::InsertArgument(stack, retval);
   return stack;
 }
@@ -133,9 +133,9 @@ ArgumentStack Item::GetAddGoldPieceValue(ArgumentStack&& args)
   ArgumentStack stack;
   int32_t retval = -1;
   if (auto *pItem = item(args))
-    {
-      retval = pItem->m_nAdditionalCost;
-    }
+  {
+    retval = pItem->m_nAdditionalCost;
+  }
   Services::Events::InsertArgument(stack, retval);
   return stack;
 }
@@ -144,10 +144,10 @@ ArgumentStack Item::SetBaseItemType(ArgumentStack&& args)
 {
   ArgumentStack stack;
   if (auto *pItem = item(args))
-    {
-      const auto bt = Services::Events::ExtractArgument<int32_t>(args);
-      pItem->m_nBaseItem = bt;
-    }
+  {
+    const auto bt = Services::Events::ExtractArgument<int32_t>(args);
+    pItem->m_nBaseItem = bt;
+  }
   return stack;
 }
 
@@ -155,58 +155,58 @@ ArgumentStack Item::SetItemAppearance(ArgumentStack&& args)
 {
   ArgumentStack stack;
   if(auto *pItem = item(args))
-     {
-        const auto type = Services::Events::ExtractArgument<int32_t>(args);
-        const auto idx   = Services::Events::ExtractArgument<int32_t>(args);
-        const auto val   = Services::Events::ExtractArgument<int32_t>(args);
-	
-        switch(type)
+  {
+    const auto type = Services::Events::ExtractArgument<int32_t>(args);
+    const auto idx   = Services::Events::ExtractArgument<int32_t>(args);
+    const auto val   = Services::Events::ExtractArgument<int32_t>(args);
+
+    switch(type)
+    {
+      case Constants::ITEM_APPR_TYPE_SIMPLE_MODEL :
+        if(val>0)
+        {       
+          pItem->m_nModelPart[0] = val;
+        }       
+        break;
+
+      case Constants::ITEM_APPR_TYPE_WEAPON_COLOR :
+        if(val>=0 && val <= 255 && idx>=0 && idx<= 5)
+        {
+          pItem->m_nLayeredTextureColors[idx] = val;
+        }
+        break;
+      case Constants::ITEM_APPR_TYPE_WEAPON_MODEL :
+        if(val>=0 && idx>=0 && idx<= 2)
+        {
+          pItem->m_nModelPart[idx] = val;
+        }
+        break;
+      case Constants::ITEM_APPR_TYPE_ARMOR_MODEL :
+        if(val>=0 && idx>=0 && idx<= 18)
+        {
+          pItem->m_nArmorModelPart[idx] = val;
+        }
+        break;
+      case Constants::ITEM_APPR_TYPE_ARMOR_COLOR :
+        if(val>=0 && val<=255 && idx>=0 && idx<= 119)
+        {
+          if(idx<=5) //1.69 colors 
           {
-	   case Constants::ITEM_APPR_TYPE_SIMPLE_MODEL :
-	     if(val>0)
-	       {		  
-		  pItem->m_nModelPart[0] = val;
-	       }	     
-	     break;
-	     
-	   case Constants::ITEM_APPR_TYPE_WEAPON_COLOR :
-	     if(val>=0 && val <= 255 && idx>=0 && idx<= 5)
-	       {
-		  pItem->m_nLayeredTextureColors[idx] = val;
-	       }
-	     break;
-	   case Constants::ITEM_APPR_TYPE_WEAPON_MODEL :
-	     if(val>=0 && idx>=0 && idx<= 2)
-	       {
-		  pItem->m_nModelPart[idx] = val;
-	       }
-	     break;
-	   case Constants::ITEM_APPR_TYPE_ARMOR_MODEL :
-	     if(val>=0 && idx>=0 && idx<= 18)
-	       {
-		  pItem->m_nArmorModelPart[idx] = val;
-	       }
-	     break;
-	   case Constants::ITEM_APPR_TYPE_ARMOR_COLOR :
-	     if(val>=0 && val<=255 && idx>=0 && idx<= 119)
-	       {
-		  if(idx<=5) //1.69 colors 
-		    {
-		       pItem->m_nLayeredTextureColors[idx]=val;
-		    }
-		  else //per-part coloring
-		    {
-		       char part = (char)((idx-6)/6);
-		       char texture = (char)(idx-6-part*6);
-		       pItem->SetLayeredTextureColorPerPart(texture, part, val);
-		    }
-	       }
-	     break;
-	  }
-     }
-   return stack;
+            pItem->m_nLayeredTextureColors[idx]=val;
+          }
+          else //per-part coloring
+          {
+            char part = (char)((idx-6)/6);
+            char texture = (char)(idx-6-part*6);
+            pItem->SetLayeredTextureColorPerPart(texture, part, val);
+          }
+        }
+        break;
+    }
+  }
+  return stack;
 }
-   
+
 ArgumentStack Item::GetEntireItemAppearance(ArgumentStack&& args)
 {
   ArgumentStack stack;
@@ -214,84 +214,89 @@ ArgumentStack Item::GetEntireItemAppearance(ArgumentStack&& args)
   char buf[4];
   int idx; 
   char part, texture;
-   
+
 
   if (auto *pItem = item(args))
+  {
+    for(idx = 0; idx < 6; idx++)
     {
-      for(idx = 0; idx < 6; idx++)
-	{
-	  sprintf(buf, "%02X", pItem->m_nLayeredTextureColors[idx]);
-	  retval << buf;
-	}
-
-      for(idx = 0; idx < 3; idx++)
-	{
-	  sprintf(buf, "%02X", pItem->m_nModelPart[idx]);
-	  retval << buf;
-	}
-      for(idx = 0; idx < 19; idx++)
-	{
-	  sprintf(buf, "%02X", pItem->m_nArmorModelPart[idx]);
-	  retval << buf;
-	}
-      for(texture=0; texture<6; texture++)
-	 {
-	    for(part=0; part<19; part++)
-	      {
-		 sprintf(buf, "%02X", pItem->GetLayeredTextureColorPerPart(texture, part));
-		 retval << buf;
-	      }
-	 }  
+      sprintf(buf, "%02X", pItem->m_nLayeredTextureColors[idx]);
+      retval << buf;
     }
 
-  Services::Events::InsertArgument(stack, retval.str());
-  return stack;
+    for(idx = 0; idx < 3; idx++)
+    {
+      sprintf(buf, "%02X", pItem->m_nModelPart[idx]);
+      retval << buf;
+    }
+    for(idx = 0; idx < 19; idx++)
+    {
+      sprintf(buf, "%02X", pItem->m_nArmorModelPart[idx]);
+      retval << buf;
+    }
+    for(texture=0; texture<6; texture++)
+    {
+      for(part=0; part<19; part++)
+      {
+       sprintf(buf, "%02X", pItem->GetLayeredTextureColorPerPart(texture, part));
+       retval << buf;
+     }
+   }  
+ }
+
+ Services::Events::InsertArgument(stack, retval.str());
+ return stack;
 }
 
 ArgumentStack Item::RestoreItemAppearance(ArgumentStack&& args)
 {
-   ArgumentStack stack;
+  ArgumentStack stack;
 
-   if (auto *pItem = item(args))
-     {
-	const auto sAppString = Services::Events::ExtractArgument<std::string>(args);
-	int idx;
-	int stringPos=0;
-	char texture, part;
-	
-	if(sAppString.length()==2*142)
-	  {
-	     for(idx = 0; idx < 6; idx++)
-	       {
-		  pItem->m_nLayeredTextureColors[idx] = std::stoul(sAppString.substr(stringPos,2), nullptr, 16);
-		  stringPos+=2;
-	       }
-	     
-	     for(idx = 0; idx < 3; idx++)
-	       {
-		  pItem->m_nModelPart[idx] = std::stoul(sAppString.substr(stringPos,2), nullptr, 16);
-		  stringPos+=2;
-	       }
-	     for(idx = 0; idx < 19; idx++)
-	       {
-		  pItem->m_nArmorModelPart[idx] = std::stoul(sAppString.substr(stringPos,2), nullptr, 16);
-		  stringPos+=2;
-	       }       
-	     for(texture=0; texture<6; texture++)
-	       {
-		  for(part=0; part<19; part++)
-		    {
-		       pItem->SetLayeredTextureColorPerPart(texture, part, std::stoul(sAppString.substr(stringPos,2), nullptr, 16));
-		       stringPos+=2;
-		    }
-	       }
-	  }
-     }
-   else
-     {
-	LOG_NOTICE("RestoreItemAppearance: invalid string length, must be 284");
-     }
-   return stack;
+  if (auto *pItem = item(args))
+  {
+    const auto sAppString = Services::Events::ExtractArgument<std::string>(args);
+    int  idx;
+    int  stringPos=0;
+    char texture, part;
+    int  strLength = sAppString.length(); 
+
+    
+    if(strLength==2*142 || strLength==2*28)
+    {
+      for(idx = 0; idx < 6; idx++)
+      {
+        pItem->m_nLayeredTextureColors[idx] = std::stoul(sAppString.substr(stringPos,2), nullptr, 16);
+        stringPos+=2;
+      }
+
+      for(idx = 0; idx < 3; idx++)
+      {
+        pItem->m_nModelPart[idx] = std::stoul(sAppString.substr(stringPos,2), nullptr, 16);
+        stringPos+=2;
+      }
+      for(idx = 0; idx < 19; idx++)
+      {
+        pItem->m_nArmorModelPart[idx] = std::stoul(sAppString.substr(stringPos,2), nullptr, 16);
+        stringPos+=2;
+      }       
+      if(strLength==2*142)
+      {
+        for(texture=0; texture<6; texture++)
+        {
+          for(part=0; part<19; part++)
+          {
+            pItem->SetLayeredTextureColorPerPart(texture, part, std::stoul(sAppString.substr(stringPos,2), nullptr, 16));
+            stringPos+=2;
+          }
+        }  
+      }
+    }
+  }
+  else
+  {
+    LOG_NOTICE("RestoreItemAppearance: invalid string length, must be 284");
+  }
+  return stack;
 }
-   
+
 }
