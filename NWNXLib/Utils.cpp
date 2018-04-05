65cfae5d4fb8063110d6a530ec8306bf86893b77
mtijanic <sherincall@gmail.com>
2018-04-05 11:37:31 +0200

    utils: Add dynamic casts for CGameObject

--- NWNXLib/Utils.cpp
+++ NWNXLib/Utils.cpp
@@ -3,6 +3,21 @@
 #include "API/CAppManager.hpp"
 #include "API/CServerExoApp.hpp"
 #include "API/CVirtualMachine.hpp"
+#include "API/Constants.hpp"
+#include "API/CNWSArea.hpp"
+#include "API/CNWSAreaOfEffectObject.hpp"
+#include "API/CNWSCreature.hpp"
+#include "API/CNWSDoor.hpp"
+#include "API/CNWSEncounter.hpp"
+#include "API/CNWSItem.hpp"
+#include "API/CNWSModule.hpp"
+#include "API/CNWSObject.hpp"
+#include "API/CNWSPlaceable.hpp"
+#include "API/CNWSSoundObject.hpp"
+#include "API/CNWSStore.hpp"
+#include "API/CNWSTrigger.hpp"
+#include "API/CNWSWaypoint.hpp"
+
 #include <sstream>
 
 namespace NWNXLib {
@@ -33,6 +48,99 @@ void ExecuteScript(const std::string& script, API::Types::ObjectID oidOwner)
     API::CExoString exoStr = script.c_str();
     API::Globals::VirtualMachine()->RunScript(&exoStr, oidOwner, 1);
 }
+
+
+API::CNWSArea* AsNWSArea(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_AREA)
+        return static_cast<API::CNWSArea*>(obj);
+    return nullptr;
+}
+API::CNWSAreaOfEffectObject* AsNWSAreaOfEffectObject(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_AREA_OF_EFFECT)
+        return static_cast<API::CNWSAreaOfEffectObject*>(obj);
+    return nullptr;
+}
+API::CNWSCreature* AsNWSCreature(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_CREATURE)
+        return static_cast<API::CNWSCreature*>(obj);
+    return nullptr;
+}
+API::CNWSDoor* AsNWSDoor(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_DOOR)
+        return static_cast<API::CNWSDoor*>(obj);
+    return nullptr;
+}
+API::CNWSEncounter* AsNWSEncounter(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_ENCOUNTER)
+        return static_cast<API::CNWSEncounter*>(obj);
+    return nullptr;
+}
+API::CNWSItem* AsNWSItem(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_ITEM)
+        return static_cast<API::CNWSItem*>(obj);
+    return nullptr;
+}
+API::CNWSModule* AsNWSModule(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_MODULE)
+        return static_cast<API::CNWSModule*>(obj);
+    return nullptr;
+}
+API::CNWSObject* AsNWSObject(API::CGameObject* obj)
+{
+    switch (obj->m_nObjectType)
+    {
+        case API::Constants::OBJECT_TYPE_AREA_OF_EFFECT:
+        case API::Constants::OBJECT_TYPE_CREATURE:
+        case API::Constants::OBJECT_TYPE_DOOR:
+        case API::Constants::OBJECT_TYPE_ENCOUNTER:
+        case API::Constants::OBJECT_TYPE_ITEM:
+        case API::Constants::OBJECT_TYPE_PLACEABLE:
+        case API::Constants::OBJECT_TYPE_SOUND:
+        case API::Constants::OBJECT_TYPE_STORE:
+        case API::Constants::OBJECT_TYPE_TRIGGER:
+        case API::Constants::OBJECT_TYPE_WAYPOINT:
+            return static_cast<API::CNWSObject*>(obj);
+    }
+    return nullptr;
+}
+API::CNWSPlaceable* AsNWSPlaceable(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_PLACEABLE)
+        return static_cast<API::CNWSPlaceable*>(obj);
+    return nullptr;
+}
+API::CNWSSoundObject* AsNWSSoundObject(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_SOUND)
+        return static_cast<API::CNWSSoundObject*>(obj);
+    return nullptr;
+}
+API::CNWSStore* AsNWSStore(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_STORE)
+        return static_cast<API::CNWSStore*>(obj);
+    return nullptr;
+}
+API::CNWSTrigger* AsNWSTrigger(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_TRIGGER)
+        return static_cast<API::CNWSTrigger*>(obj);
+    return nullptr;
+}
+API::CNWSWaypoint* AsNWSWaypoint(API::CGameObject* obj)
+{
+    if (obj->m_nObjectType == API::Constants::OBJECT_TYPE_WAYPOINT)
+        return static_cast<API::CNWSWaypoint*>(obj);
+    return nullptr;
+}
+
 
 }
 }
--- NWNXLib/Utils.hpp
+++ NWNXLib/Utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "API/Types.hpp"
+#include "API/CGameObject.hpp"
 #include <string>
 
 
@@ -11,6 +12,22 @@ std::string ObjectIDToString(const API::Types::ObjectID id);
 
 std::string GetCurrentScript();
 void ExecuteScript(const std::string& script, API::Types::ObjectID oidOwner);
+
+// Since there's no RTTI, and NWN's dynamic casts don't work in NWNX.
+// These return nullptr if the object type isn't right.
+API::CNWSArea*               AsNWSArea(API::CGameObject* obj);
+API::CNWSAreaOfEffectObject* AsNWSAreaOfEffectObject(API::CGameObject* obj);
+API::CNWSCreature*           AsNWSCreature(API::CGameObject* obj);
+API::CNWSDoor*               AsNWSDoor(API::CGameObject* obj);
+API::CNWSEncounter*          AsNWSEncounter(API::CGameObject* obj);
+API::CNWSItem*               AsNWSItem(API::CGameObject* obj);
+API::CNWSModule*             AsNWSModule(API::CGameObject* obj);
+API::CNWSObject*             AsNWSObject(API::CGameObject* obj);
+API::CNWSPlaceable*          AsNWSPlaceable(API::CGameObject* obj);
+API::CNWSSoundObject*        AsNWSSoundObject(API::CGameObject* obj);
+API::CNWSStore*              AsNWSStore(API::CGameObject* obj);
+API::CNWSTrigger*            AsNWSTrigger(API::CGameObject* obj);
+API::CNWSWaypoint*           AsNWSWaypoint(API::CGameObject* obj);
 
 }
 
