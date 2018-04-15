54522fa743fa79524036dfdf2a8bf606c98470d5
Liareth <liarethnwn@gmail.com>
2018-04-16 00:18:34 +0200

    WebHook: Initial implementation of Slack-formatted JSON WebHook.

---
+++ Plugins/WebHook/NWScript/nwnx_webhook.nss
@@ -0,0 +1,14 @@
+#include "nwnx"
+
+// Over HTTPS, sends message (formatted as application/json in slack compatible format) to hostpath.
+// For a Discord webhook, the call to this function would look something like:
+//     NWNX_WebHook_SendWebHookHTTPS("discordapp.com", "/api/webhooks/biglongwebhooktoken/slack", "This is my message!");
+void NWNX_WebHook_SendWebHookHTTPS(string host, string path, string message);
+
+void NWNX_WebHook_SendWebHookHTTPS(string host, string path, string message)
+{
+    NWNX_PushArgumentString("NWNX_WebHook", "SEND_WEBHOOK_HTTPS", message);
+    NWNX_PushArgumentString("NWNX_WebHook", "SEND_WEBHOOK_HTTPS", path);
+    NWNX_PushArgumentString("NWNX_WebHook", "SEND_WEBHOOK_HTTPS", host);
+    NWNX_CallFunction("NWNX_WebHook", "SEND_WEBHOOK_HTTPS");
+}
---
+++ Plugins/WebHook/WebHook.cpp
@@ -0,0 +1,86 @@
+#include "WebHook.hpp"
+#include "API/Version.hpp"
+
+#include "External/httplib.h"
+
+using namespace NWNXLib;
+
+static ViewPtr<WebHook::WebHook> g_plugin;
+
+NWNX_PLUGIN_ENTRY Plugin::Info* PluginInfo()
+{
+    return new Plugin::Info
+    {
+        "WebHook",
+        "Users can send Slack-formatted HTTP/HTTPS webhooks with this plugin.",
+        "Liareth",
+        "liarethnwn@gmail.com",
+        1,
+        true
+    };
+}
+
+NWNX_PLUGIN_ENTRY Plugin* PluginLoad(Plugin::CreateParams params)
+{
+    g_plugin = new WebHook::WebHook(params);
+    return g_plugin;
+}
+
+using namespace NWNXLib::Services;
+
+namespace WebHook {
+
+WebHook::WebHook(const Plugin::CreateParams& params)
+    : Plugin(params)
+{
+    GetServices()->m_events->RegisterEvent("SEND_WEBHOOK_HTTPS", &OnSendWebhookHTTPS);
+}
+
+WebHook::~WebHook()
+{ }
+
+Events::ArgumentStack WebHook::OnSendWebhookHTTPS(Events::ArgumentStack&& args)
+{
+    std::string host = Services::Events::ExtractArgument<std::string>(args);
+    std::string path = Services::Events::ExtractArgument<std::string>(args);
+    std::string message = Services::Events::ExtractArgument<std::string>(args);
+
+    // Make sure to escape any quotes.
+    // There are probably more things we have to escape too.
+    size_t pos = 0;
+    static const std::string s_Find = "\"";
+    static const std::string s_Replace = "\\\"";
+    while ((pos = message.find(s_Find, pos)) != std::string::npos)
+    {
+         message.replace(pos, s_Find.length(), s_Replace);
+         pos += s_Replace.length();
+    }
+
+    message = "{\"text\":\"" + message + "\"}";
+
+    httplib::SSLClient cli(host.c_str(), 443);
+    auto res = cli.post(path.c_str(), message, "application/json");
+
+    if (res)
+    {
+        if (res->status == 200)
+        {
+            LOG_INFO("Sent webhook '%s' to '%s%s'.",
+                message.c_str(), host.c_str(), path.c_str());
+        }
+        else
+        {
+            LOG_WARNING("Status code %d when seding '%s' to '%s%s'.",
+                res->status, message.c_str(), host.c_str(), path.c_str());
+        }
+    }
+    else
+    {
+        LOG_WARNING("Failed to send WebHook (HTTPS) to '%s%s'.",
+            host.c_str(), path.c_str());
+    }
+
+    return Events::ArgumentStack();
+}
+
+}
