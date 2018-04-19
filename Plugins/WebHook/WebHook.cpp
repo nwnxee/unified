34a8d95c1ffd281ad5ddfdbc620149c4fe39b884
Bhaal <bhaal@marcaargentea.net>
2018-04-19 23:44:48 +0200

    Added missing new lines.

--- Plugins/WebHook/WebHook.cpp
+++ Plugins/WebHook/WebHook.cpp
@@ -121,10 +121,12 @@ std::string WebHook::iso_8859_1_to_utf8(std::string &str)
     for (std::string::iterator it = str.begin(); it != str.end(); ++it)
     {
         uint8_t ch = *it;
-        if (ch < 0x80) {
+        if (ch < 0x80) 
+        {
             strOut.push_back(ch);
         }
-        else {
+        else 
+        {
             strOut.push_back(0xc0 | ch >> 6);
             strOut.push_back(0x80 | (ch & 0x3f));
         }
