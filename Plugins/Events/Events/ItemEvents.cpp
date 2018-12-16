ce157bd82cb9ff12ddbdb651b4ccaf6e7cd6249c
Daztek <daztek@gmail.com>
2018-12-16 23:32:40 +0100

    On Use Item Event Skip Fixes

--- Plugins/Events/Events/ItemEvents.cpp
+++ Plugins/Events/Events/ItemEvents.cpp
@@ -12,35 +12,51 @@ static Hooking::FunctionHook* m_UseItemHook = nullptr;
 
 ItemEvents::ItemEvents(ViewPtr<Services::HooksProxy> hooker)
 {
-    hooker->RequestExclusiveHook<API::Functions::CNWSCreature__UseItem, void, API::CNWSCreature*, API::Types::ObjectID, uint8_t,
+    hooker->RequestExclusiveHook<API::Functions::CNWSCreature__UseItem, int32_t, API::CNWSCreature*, API::Types::ObjectID, uint8_t,
         uint8_t, API::Types::ObjectID, API::Vector, API::Types::ObjectID>(&UseItemHook);
 
     m_UseItemHook = hooker->FindHookByAddress(API::Functions::CNWSCreature__UseItem);
 }
 
-void ItemEvents::UseItemHook(
+int32_t ItemEvents::UseItemHook(
     API::CNWSCreature* thisPtr, 
     API::Types::ObjectID item, 
     uint8_t propIndex,
-    uint8_t unusedInt,
+    uint8_t subPropIndex,
     API::Types::ObjectID target, 
-    API::Vector unusedVector, 
-    API::Types::ObjectID unusedObjectId)
+    API::Vector targetPosition, 
+    API::Types::ObjectID area)
 {
+    int32_t retVal;
+    
     Events::PushEventData("ITEM_OBJECT_ID", Utils::ObjectIDToString(item));
     Events::PushEventData("TARGET_OBJECT_ID", Utils::ObjectIDToString(target));
     Events::PushEventData("ITEM_PROPERTY_INDEX", std::to_string(propIndex));
+    Events::PushEventData("ITEM_SUB_PROPERTY_INDEX", std::to_string(subPropIndex));
+    Events::PushEventData("TARGET_POSITION_X", std::to_string(targetPosition.x));
+    Events::PushEventData("TARGET_POSITION_Y", std::to_string(targetPosition.y));
+    Events::PushEventData("TARGET_POSITION_Z", std::to_string(targetPosition.z));    
     
     if (Events::SignalEvent("NWNX_ON_USE_ITEM_BEFORE", thisPtr->m_idSelf))
     {
-        m_UseItemHook->CallOriginal<void>(thisPtr, item, propIndex, unusedInt, target, unusedVector, unusedObjectId);
+        retVal = m_UseItemHook->CallOriginal<int32_t>(thisPtr, item, propIndex, subPropIndex, target, targetPosition, area);
+    }
+    else
+    {
+        retVal = false;    
     }
 
     Events::PushEventData("ITEM_OBJECT_ID", Utils::ObjectIDToString(item));
     Events::PushEventData("TARGET_OBJECT_ID", Utils::ObjectIDToString(target));
     Events::PushEventData("ITEM_PROPERTY_INDEX", std::to_string(propIndex));
+    Events::PushEventData("ITEM_SUB_PROPERTY_INDEX", std::to_string(subPropIndex));
+    Events::PushEventData("TARGET_POSITION_X", std::to_string(targetPosition.x));
+    Events::PushEventData("TARGET_POSITION_Y", std::to_string(targetPosition.y));
+    Events::PushEventData("TARGET_POSITION_Z", std::to_string(targetPosition.z));    
 
     Events::SignalEvent("NWNX_ON_USE_ITEM_AFTER", thisPtr->m_idSelf);
+
+    return retVal;
 }
 
 }
