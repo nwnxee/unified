268d07c131292e75f420aa81b80a075aeaf4604d
mtijanic <sherincall@gmail.com>
2019-01-15 11:27:52 +0100

    util: packing and unpacking effects!

--- Plugins/Util/NWScript/nwnx_util.nss
+++ Plugins/Util/NWScript/nwnx_util.nss
@@ -10,6 +10,55 @@ string NWNX_Util_GetAsciiTableString();
 int NWNX_Util_Hash(string str);
 
 
+struct NWNX_Effect
+{
+    int nType;
+    int nSubType;
+
+    float fDuration;
+    int nExpiryCalendarDay;
+    int nExpiryTimeOfDay;
+
+    object oCreator;
+    int nSpellId;
+    int bExpose;
+    int bShowIcon;
+    int nCasterLevel;
+
+    effect eLinkLeft;
+    int bLinkLeftValid;
+    effect eLinkRight;
+    int eLinkRightValid;
+
+    int nNumIntegers;
+    int nParam0;
+    int nParam1;
+    int nParam2;
+    int nParam3;
+    int nParam4;
+    int nParam5;
+    int nParam6;
+    int nParam7;
+    float fParam0;
+    float fParam1;
+    float fParam2;
+    float fParam3;
+    string sParam0;
+    string sParam1;
+    string sParam2;
+    string sParam3;
+    string sParam4;
+    string sParam5;
+    object oParam0;
+    object oParam1;
+    object oParam2;
+    object oParam3;
+
+    string sTag;
+};
+
+
+
 const string NWNX_Util = "NWNX_Util";
 
 
@@ -34,3 +83,109 @@ int NWNX_Util_Hash(string str)
     return NWNX_GetReturnValueInt(NWNX_Util, sFunc);
 }
 
+
+struct NWNX_Effect NWNX_Util_UnpackEffect(effect e)
+{
+    string sFunc = "UnpackEffect";
+
+    NWNX_PushArgumentEffect(NWNX_Util, sFunc, e);
+    NWNX_CallFunction(NWNX_Util, sFunc);
+
+    struct NWNX_Effect n;
+    n.sTag = NWNX_GetReturnValueString(NWNX_Util, sFunc);
+
+    n.oParam3 = NWNX_GetReturnValueObject(NWNX_Util, sFunc);
+    n.oParam2 = NWNX_GetReturnValueObject(NWNX_Util, sFunc);
+    n.oParam1 = NWNX_GetReturnValueObject(NWNX_Util, sFunc);
+    n.oParam0 = NWNX_GetReturnValueObject(NWNX_Util, sFunc);
+    n.sParam5 = NWNX_GetReturnValueString(NWNX_Util, sFunc);
+    n.sParam4 = NWNX_GetReturnValueString(NWNX_Util, sFunc);
+    n.sParam3 = NWNX_GetReturnValueString(NWNX_Util, sFunc);
+    n.sParam2 = NWNX_GetReturnValueString(NWNX_Util, sFunc);
+    n.sParam1 = NWNX_GetReturnValueString(NWNX_Util, sFunc);
+    n.sParam0 = NWNX_GetReturnValueString(NWNX_Util, sFunc);
+    n.fParam3 = NWNX_GetReturnValueFloat(NWNX_Util, sFunc);
+    n.fParam2 = NWNX_GetReturnValueFloat(NWNX_Util, sFunc);
+    n.fParam1 = NWNX_GetReturnValueFloat(NWNX_Util, sFunc);
+    n.fParam0 = NWNX_GetReturnValueFloat(NWNX_Util, sFunc);
+    n.nParam7 = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nParam6 = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nParam5 = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nParam4 = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nParam3 = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nParam2 = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nParam1 = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nParam0 = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nNumIntegers = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+
+    n.eLinkRightValid = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.eLinkRight = NWNX_GetReturnValueEffect(NWNX_Util, sFunc);
+    n.bLinkLeftValid = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.eLinkLeft = NWNX_GetReturnValueEffect(NWNX_Util, sFunc);
+
+    n.nCasterLevel = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.bShowIcon = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.bExpose = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nSpellId = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.oCreator = NWNX_GetReturnValueObject(NWNX_Util, sFunc);
+
+    n.nExpiryTimeOfDay = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nExpiryCalendarDay = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.fDuration = NWNX_GetReturnValueFloat(NWNX_Util, sFunc);
+
+    n.nSubType = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+    n.nType = NWNX_GetReturnValueInt(NWNX_Util, sFunc);
+
+    return n;
+}
+effect NWNX_Util_PackEffect(struct NWNX_Effect e)
+{
+    string sFunc = "PackEffect";
+
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nType);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nSubType);
+
+    NWNX_PushArgumentFloat(NWNX_Util, sFunc, e.fDuration);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nExpiryCalendarDay);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nExpiryTimeOfDay);
+
+    NWNX_PushArgumentObject(NWNX_Util, sFunc, e.oCreator);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nSpellId);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.bExpose);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.bShowIcon);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nCasterLevel);
+
+    NWNX_PushArgumentEffect(NWNX_Util, sFunc, e.eLinkLeft);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.bLinkLeftValid);
+    NWNX_PushArgumentEffect(NWNX_Util, sFunc, e.eLinkRight);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.eLinkRightValid);
+
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nNumIntegers);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nParam0);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nParam1);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nParam2);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nParam3);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nParam4);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nParam5);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nParam6);
+    NWNX_PushArgumentInt(NWNX_Util, sFunc, e.nParam7);
+    NWNX_PushArgumentFloat(NWNX_Util, sFunc, e.fParam0);
+    NWNX_PushArgumentFloat(NWNX_Util, sFunc, e.fParam1);
+    NWNX_PushArgumentFloat(NWNX_Util, sFunc, e.fParam2);
+    NWNX_PushArgumentFloat(NWNX_Util, sFunc, e.fParam3);
+    NWNX_PushArgumentString(NWNX_Util, sFunc, e.sParam0);
+    NWNX_PushArgumentString(NWNX_Util, sFunc, e.sParam1);
+    NWNX_PushArgumentString(NWNX_Util, sFunc, e.sParam2);
+    NWNX_PushArgumentString(NWNX_Util, sFunc, e.sParam3);
+    NWNX_PushArgumentString(NWNX_Util, sFunc, e.sParam4);
+    NWNX_PushArgumentString(NWNX_Util, sFunc, e.sParam5);
+    NWNX_PushArgumentObject(NWNX_Util, sFunc, e.oParam0);
+    NWNX_PushArgumentObject(NWNX_Util, sFunc, e.oParam1);
+    NWNX_PushArgumentObject(NWNX_Util, sFunc, e.oParam2);
+    NWNX_PushArgumentObject(NWNX_Util, sFunc, e.oParam3);
+
+    NWNX_PushArgumentString(NWNX_Util, sFunc, e.sTag);
+
+    NWNX_CallFunction(NWNX_Util, sFunc);
+    return NWNX_GetReturnValueEffect(NWNX_Util, sFunc);
+}
--- Plugins/Util/Util.cpp
+++ Plugins/Util/Util.cpp
@@ -45,6 +45,8 @@ Util::Util(const Plugin::CreateParams& params)
     REGISTER(GetCurrentScriptName);
     REGISTER(GetAsciiTableString);
     REGISTER(Hash);
+    REGISTER(PackEffect);
+    REGISTER(UnpackEffect);
 
 #undef REGISTER
 
@@ -85,7 +87,6 @@ ArgumentStack Util::GetAsciiTableString(ArgumentStack&& args)
         for (size_t i = 1; i < sizeof(table); i++)
             table[i] = i;
     }
-    std::string retval = "";
 
     Services::Events::InsertArgument(stack, std::string(table));
     return stack;
@@ -97,6 +98,119 @@ ArgumentStack Util::Hash(ArgumentStack&& args)
     const auto str = Services::Events::ExtractArgument<std::string>(args);
 
     Services::Events::InsertArgument(stack, (int32_t)std::hash<std::string>{}(str));
+    return stack;
+}
+
+ArgumentStack Util::PackEffect(ArgumentStack&& args)
+{
+    ArgumentStack stack;
+    API::CGameEffect *eff = new API::CGameEffect();
+
+    eff->m_sCustomTag = Services::Events::ExtractArgument<std::string>(args).c_str();
+
+    eff->m_oidParamObjectID[3] = Services::Events::ExtractArgument<API::Types::ObjectID>(args);
+    eff->m_oidParamObjectID[2] = Services::Events::ExtractArgument<API::Types::ObjectID>(args);
+    eff->m_oidParamObjectID[1] = Services::Events::ExtractArgument<API::Types::ObjectID>(args);
+    eff->m_oidParamObjectID[0] = Services::Events::ExtractArgument<API::Types::ObjectID>(args);
+
+    eff->m_sParamString[5] = Services::Events::ExtractArgument<std::string>(args).c_str();
+    eff->m_sParamString[4] = Services::Events::ExtractArgument<std::string>(args).c_str();
+    eff->m_sParamString[3] = Services::Events::ExtractArgument<std::string>(args).c_str();
+    eff->m_sParamString[2] = Services::Events::ExtractArgument<std::string>(args).c_str();
+    eff->m_sParamString[1] = Services::Events::ExtractArgument<std::string>(args).c_str();
+    eff->m_sParamString[0] = Services::Events::ExtractArgument<std::string>(args).c_str();
+
+    eff->m_nParamFloat[3] = Services::Events::ExtractArgument<float>(args);
+    eff->m_nParamFloat[2] = Services::Events::ExtractArgument<float>(args);
+    eff->m_nParamFloat[1] = Services::Events::ExtractArgument<float>(args);
+    eff->m_nParamFloat[0] = Services::Events::ExtractArgument<float>(args);
+
+    eff->SetNumIntegers(8); // allocate array
+    eff->m_nParamInteger[7] = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nParamInteger[6] = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nParamInteger[5] = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nParamInteger[4] = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nParamInteger[3] = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nParamInteger[2] = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nParamInteger[1] = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nParamInteger[0] = Services::Events::ExtractArgument<int32_t>(args);
+    // Overwrite num integers from 8
+    eff->m_nNumIntegers = Services::Events::ExtractArgument<int32_t>(args);
+
+    int32_t bRightLinkValid = Services::Events::ExtractArgument<int32_t>(args);
+    API::CGameEffect *pRightLink = Services::Events::ExtractArgument<API::CGameEffect*>(args);
+    eff->m_pLinkRight = (bRightLinkValid) ? pRightLink : nullptr;
+
+    int32_t bLeftLinkValid = Services::Events::ExtractArgument<int32_t>(args);
+    API::CGameEffect *pLeftLink = Services::Events::ExtractArgument<API::CGameEffect*>(args);
+    eff->m_pLinkLeft = (bLeftLinkValid) ? pLeftLink : nullptr;
+
+    eff->m_nCasterLevel       = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_bShowIcon          = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_bExpose            = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nSpellId           = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_oidCreator         = Services::Events::ExtractArgument<API::Types::ObjectID>(args);
+    eff->m_nExpiryTimeOfDay   = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nExpiryCalendarDay = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_fDuration          = Services::Events::ExtractArgument<float>(args);
+    eff->m_nSubType           = Services::Events::ExtractArgument<int32_t>(args);
+    eff->m_nType              = Services::Events::ExtractArgument<int32_t>(args);
+
+    if (bLeftLinkValid || bRightLinkValid)
+        eff->UpdateLinked();
+
+    Services::Events::InsertArgument(stack, eff);
+    return stack;
+}
+ArgumentStack Util::UnpackEffect(ArgumentStack&& args)
+{
+    ArgumentStack stack;
+    auto eff = Services::Events::ExtractArgument<API::CGameEffect*>(args);
+
+    Services::Events::InsertArgument(stack, (int32_t)eff->m_nType);
+    Services::Events::InsertArgument(stack, (int32_t)eff->m_nSubType);
+    Services::Events::InsertArgument(stack, (float)eff->m_fDuration);
+    Services::Events::InsertArgument(stack, (int32_t)eff->m_nExpiryCalendarDay);
+    Services::Events::InsertArgument(stack, (int32_t)eff->m_nExpiryTimeOfDay);
+    Services::Events::InsertArgument(stack, (API::Types::ObjectID)eff->m_oidCreator);
+    Services::Events::InsertArgument(stack, (int32_t)eff->m_nSpellId);
+    Services::Events::InsertArgument(stack, (int32_t)eff->m_bExpose);
+    Services::Events::InsertArgument(stack, (int32_t)eff->m_bShowIcon);
+    Services::Events::InsertArgument(stack, (int32_t)eff->m_nCasterLevel);
+    Services::Events::InsertArgument(stack, (API::CGameEffect*)eff->m_pLinkLeft);
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_pLinkLeft != nullptr));
+    Services::Events::InsertArgument(stack, (API::CGameEffect*)eff->m_pLinkRight);
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_pLinkRight != nullptr));
+
+    Services::Events::InsertArgument(stack, (int32_t)eff->m_nNumIntegers);
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_nNumIntegers > 0 ? eff->m_nParamInteger[0] : -1));
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_nNumIntegers > 1 ? eff->m_nParamInteger[1] : -1));
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_nNumIntegers > 2 ? eff->m_nParamInteger[2] : -1));
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_nNumIntegers > 3 ? eff->m_nParamInteger[3] : -1));
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_nNumIntegers > 4 ? eff->m_nParamInteger[4] : -1));
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_nNumIntegers > 5 ? eff->m_nParamInteger[5] : -1));
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_nNumIntegers > 6 ? eff->m_nParamInteger[6] : -1));
+    Services::Events::InsertArgument(stack, (int32_t)(eff->m_nNumIntegers > 7 ? eff->m_nParamInteger[7] : -1));
+
+    Services::Events::InsertArgument(stack, (float)eff->m_nParamFloat[0]);
+    Services::Events::InsertArgument(stack, (float)eff->m_nParamFloat[1]);
+    Services::Events::InsertArgument(stack, (float)eff->m_nParamFloat[2]);
+    Services::Events::InsertArgument(stack, (float)eff->m_nParamFloat[3]);
+
+    Services::Events::InsertArgument(stack, std::string(eff->m_sParamString[0].CStr()));
+    Services::Events::InsertArgument(stack, std::string(eff->m_sParamString[1].CStr()));
+    Services::Events::InsertArgument(stack, std::string(eff->m_sParamString[2].CStr()));
+    Services::Events::InsertArgument(stack, std::string(eff->m_sParamString[3].CStr()));
+    Services::Events::InsertArgument(stack, std::string(eff->m_sParamString[4].CStr()));
+    Services::Events::InsertArgument(stack, std::string(eff->m_sParamString[5].CStr()));
+
+    Services::Events::InsertArgument(stack, (API::Types::ObjectID)eff->m_oidParamObjectID[0]);
+    Services::Events::InsertArgument(stack, (API::Types::ObjectID)eff->m_oidParamObjectID[1]);
+    Services::Events::InsertArgument(stack, (API::Types::ObjectID)eff->m_oidParamObjectID[2]);
+    Services::Events::InsertArgument(stack, (API::Types::ObjectID)eff->m_oidParamObjectID[3]);
+
+    Services::Events::InsertArgument(stack, std::string(eff->m_sCustomTag.CStr()));
+    delete eff;
     return stack;
 }
 
