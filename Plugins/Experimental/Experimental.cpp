cd491e3f8a08836091faf581ba8accb890ecdddc
Daztek <daztek@gmail.com>
2019-02-20 11:50:53 +0100

    Experimental: Fix even more whitespace issues

--- Plugins/Experimental/Experimental.cpp
+++ Plugins/Experimental/Experimental.cpp
@@ -11,14 +11,14 @@ static ViewPtr<Experimental::Experimental> g_plugin;
 NWNX_PLUGIN_ENTRY Plugin::Info* PluginInfo()
 {
     return new Plugin::Info
-            {
-                "Experimental",
-                "Adds experimental functionality that's not really useful for the general user.",
-                "Daz",
-                "daztek@gmail.com",
-                1,
-                true
-            };
+    {
+        "Experimental",
+        "Adds experimental functionality that's not really useful for the general user.",
+        "Daz",
+        "daztek@gmail.com",
+        1,
+        true
+    };
 }
 
 NWNX_PLUGIN_ENTRY Plugin* PluginLoad(Plugin::CreateParams params)
