75fa7be9cff4dfac89c4695e50243dc0a14d8aaf
mtijanic <sherincall@gmail.com>
2019-02-23 16:41:08 +0100

    Some functions throw exceptions that are not runtime_error. Catch those too

--- NWNXLib/Services/Events/Events.cpp
+++ NWNXLib/Services/Events/Events.cpp
@@ -38,11 +38,11 @@ void Events::Call(const std::string& pluginName, const std::string& eventName)
     {
         LOG_DEBUG("Calling event handler. Event '%s', Plugin: '%s'.",
             eventName.c_str(), pluginName.c_str());
-        try 
+        try
         {
             event->m_returns = event->m_callback(std::move(event->m_arguments));
         }
-        catch (const std::runtime_error& err)
+        catch (const std::exception& err)
         {
             LOG_ERROR("Plugin '%s' failed event '%s'. Error: %s", pluginName.c_str(), eventName.c_str(), err.what());
         }
