0ac7b75b685a87791a6fc3e6fcd572dbbe404c3b
Henning Koehler <henning.koehler.nz@gmail.com>
2019-03-08 14:37:25 +1300

    added attack event hook and functions to modify damage dealt; some refactoring of damage event

--- Plugins/Damage/Damage.hpp
+++ Plugins/Damage/Damage.hpp
@@ -12,6 +12,16 @@ struct DamageDataStr
     int32_t  vDamage[13];
 };
 
+struct AttackDataStr
+{
+    uint32_t oidTarget;
+    int16_t  vDamage[13];
+    uint8_t  nAttackNumber;
+    uint8_t  nAttackResult;
+    uint8_t  bSneakAttack;
+    uint8_t  bOffhand;
+};
+
 namespace Damage {
 
 class Damage : public NWNXLib::Plugin
@@ -21,16 +31,22 @@ public:
     virtual ~Damage();
 
 private:
-    ArgumentStack SetDamageEventScript(ArgumentStack&& args);
-    ArgumentStack GetEventData(ArgumentStack&& args);
-    ArgumentStack SetEventData(ArgumentStack&& args);
-
-    NWNXLib::Hooking::FunctionHook* m_OnApplyDamageHook;
+    ArgumentStack SetEventScript(ArgumentStack&& args);
+    ArgumentStack GetDamageEventData(ArgumentStack&& args);
+    ArgumentStack SetDamageEventData(ArgumentStack&& args);
+    ArgumentStack GetAttackEventData(ArgumentStack&& args);
+    ArgumentStack SetAttackEventData(ArgumentStack&& args);
 
     static int32_t OnApplyDamage(NWNXLib::API::CNWSEffectListHandler *pThis, NWNXLib::API::CNWSObject *pObject, NWNXLib::API::CGameEffect *pEffect, bool bLoadingGame);
+    static void OnCombatAttack(NWNXLib::API::CNWSCombatRound *pThis, uint8_t attackNumber);
 
-    std::string m_DamageScript;
+    static std::string GetEventScript(NWNXLib::API::CNWSObject *pObject, const std::string &event);
+
+    std::unordered_map<std::string,NWNXLib::Hooking::FunctionHook*> m_EventHooks;
+    std::unordered_map<std::string,std::string> m_EventScripts;
+
     DamageDataStr m_DamageData;
+    AttackDataStr m_AttackData;
 };
 
 }
