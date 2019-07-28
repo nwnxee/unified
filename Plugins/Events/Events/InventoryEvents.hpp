e78f7060e1c93960f2d8934a7f7f5398e235e6a6
Daztek <daztek@gmail.com>
2019-07-28 13:27:55 +0200

    Events: add {Add|Remove}Gold InventoryEvents

--- Plugins/Events/Events/InventoryEvents.hpp
+++ Plugins/Events/Events/InventoryEvents.hpp
@@ -17,6 +17,8 @@ private:
     static int32_t HandlePlayerToServerGuiInventoryMessageHook(NWNXLib::API::CNWSMessage*, NWNXLib::API::CNWSPlayer*, uint8_t);
     static int32_t AddItemHook(NWNXLib::API::CItemRepository*, NWNXLib::API::CNWSItem**, uint8_t, uint8_t, int32_t, int32_t);
     static void RemoveItemHook(NWNXLib::Services::Hooks::CallType, NWNXLib::API::CItemRepository*, NWNXLib::API::CNWSItem*);
+    static void AddGoldHook(NWNXLib::API::CNWSCreature*, int32_t, int32_t);
+    static void RemoveGoldHook(NWNXLib::API::CNWSCreature*, int32_t, int32_t);
 };
 
 }
