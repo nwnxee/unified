a22e82ca26cec3a42c9222f14d6bdf81253e05c2
jmd <joshua.m.dean@gmail.com>
2019-09-27 20:19:37 -0700

    NWNXLib/Plugin: Change Info constructor parameters
    Given the actual usage of plugins to pass const char* in these parameters, there is no point in std::string&& and even if they didn't, the types are still questionable.
    
    Make it explicit that std::strings are getting constructed one way or the other.  One could argue that these reasonably could be const char* parameters.  I don't know the code base well enough tho.
    
    The use of std::forward here is just not right, even if it works.

--- NWNXLib/Plugin.cpp
+++ NWNXLib/Plugin.cpp
@@ -11,18 +11,18 @@ Plugin::~Plugin()
 {
 }
 
-Plugin::Info::Info(std::string&& name,
-    std::string&& description,
-    std::string&& author,
-    std::string&& contact,
+Plugin::Info::Info(std::string name,
+    std::string description,
+    std::string author,
+    std::string contact,
     const Version version,
     const bool hotswap,
     const uint32_t flags,
     const Version targetVersion)
-    : m_name(std::forward<std::string>(name)),
-      m_description(std::forward<std::string>(description)),
-      m_author(std::forward<std::string>(author)),
-      m_contact(std::forward<std::string>(contact)),
+    : m_name(std::move(name)),
+      m_description(std::move(description)),
+      m_author(std::move(author)),
+      m_contact(std::move(contact)),
       m_version(version),
       m_hotswap(hotswap),
       m_flags(flags),
