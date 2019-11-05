65d10c524b86a5bf9b7cd873f06f8c53b152e63a
Daztek <daztek@gmail.com>
2019-11-05 21:58:43 +0100

    FunctionHook: add missing ASSERT

--- NWNXLib/Hooking/FunctionHook.cpp
+++ NWNXLib/Hooking/FunctionHook.cpp
@@ -1,26 +1,24 @@
 #include "Hooking/FunctionHook.hpp"
-#include "Platform/ASLR.hpp"
 #include "Assert.hpp"
 
 namespace NWNXLib {
 
 namespace Hooking {
 
-using namespace NWNXLib::Platform;
-
 FunctionHook::FunctionHook(uintptr_t originalFunction, uintptr_t newFunction)
 {
     int rv;
 
     m_funchook = funchook_create();
+    ASSERT(m_funchook);
 
-    rv = funchook_prepare(m_funchook, (void **) &originalFunction, (void *) newFunction);
+    rv = funchook_prepare(m_funchook, (void **)&originalFunction, (void *)newFunction);
     ASSERT(rv == FUNCHOOK_ERROR_SUCCESS);
 
     rv = funchook_install(m_funchook, 0);
     ASSERT(rv == FUNCHOOK_ERROR_SUCCESS);
 
-    m_trampoline = (void *) originalFunction;
+    m_trampoline = (void *)originalFunction;
     ASSERT(m_trampoline);
 }
 
