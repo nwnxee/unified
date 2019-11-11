c121d084f5367954d8c689a20a916e437fa1823b
ci-build <ci-build@nwnx.io>
2019-11-11 17:43:47 +0000

    [skip ci] Updates

--- dir_8a36ff99015379f5b138a8336ad5e5a6.js
+++ dir_8a36ff99015379f5b138a8336ad5e5a6.js
@@ -1,15 +1,15 @@
 var dir_8a36ff99015379f5b138a8336ad5e5a6 =
 [
     [ "Sample", "group__nwnx.html#autotoc_md24", null ],
-    [ "SetPersistentLocation()", "group__player.html#autotoc_md56", null ],
-    [ "Example 1", "group__skillranks.html#autotoc_md140", null ],
-    [ "Example 2", "group__skillranks.html#autotoc_md141", null ],
-    [ "Example 3", "group__skillranks.html#autotoc_md142", null ],
-    [ "Example 4", "group__skillranks.html#autotoc_md143", null ],
-    [ "Example 5", "group__skillranks.html#autotoc_md144", null ],
-    [ "Example 6", "group__skillranks.html#autotoc_md145", null ],
-    [ "Example 1", "group__webhook.html#autotoc_md172", null ],
-    [ "Example 2", "group__webhook.html#autotoc_md173", null ],
-    [ "Example 3", "group__webhook.html#autotoc_md174", null ],
-    [ "Example 4", "group__webhook.html#autotoc_md175", null ]
+    [ "SetPersistentLocation()", "group__player.html#autotoc_md112", null ],
+    [ "Example 1", "group__skillranks.html#autotoc_md196", null ],
+    [ "Example 2", "group__skillranks.html#autotoc_md197", null ],
+    [ "Example 3", "group__skillranks.html#autotoc_md198", null ],
+    [ "Example 4", "group__skillranks.html#autotoc_md199", null ],
+    [ "Example 5", "group__skillranks.html#autotoc_md200", null ],
+    [ "Example 6", "group__skillranks.html#autotoc_md201", null ],
+    [ "Example 1", "group__webhook.html#autotoc_md228", null ],
+    [ "Example 2", "group__webhook.html#autotoc_md229", null ],
+    [ "Example 3", "group__webhook.html#autotoc_md230", null ],
+    [ "Example 4", "group__webhook.html#autotoc_md231", null ]
 ];
