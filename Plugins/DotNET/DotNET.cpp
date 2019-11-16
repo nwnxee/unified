5be00eadabd0ba61280b4f1fab0ea6b0c5b9c9c2
mtijanic <sherincall@gmail.com>
2019-11-16 19:50:56 +0100

    dotnet: base implementation, WIP

---
+++ Plugins/DotNET/DotNET.cpp
@@ -0,0 +1,314 @@
+#include "DotNET.hpp"
+#include "API/CNWVirtualMachineCommands.hpp"
+#include "API/CVirtualMachine.hpp"
+#include "API/CVirtualMachineScript.hpp"
+#include "API/Functions.hpp"
+#include "API/Version.hpp"
+#include "Services/Config/Config.hpp"
+#include "Services/Messaging/Messaging.hpp"
+#include "Services/Hooks/Hooks.hpp"
+
+#include <algorithm>
+#include <cstring>
+#include <chrono>
+#include <string>
+
+#include "sdk/coreclr_delegates.h"
+#include "sdk/hostfxr.h"
+#include <dlfcn.h>
+#include <limits.h>
+
+using namespace NWNXLib;
+using namespace NWNXLib::API;
+
+static ViewPtr<DotNET::DotNET> g_plugin;
+
+NWNX_PLUGIN_ENTRY Plugin::Info* PluginInfo()
+{
+    return new Plugin::Info
+    {
+        "DotNET",
+        ".NET nwscript bindings and more",
+        "mtijanic",
+        "sherincall@gmail.com",
+        1,
+        false
+    };
+}
+
+NWNX_PLUGIN_ENTRY Plugin* PluginLoad(Plugin::CreateParams params)
+{
+    return new DotNET::DotNET(params);
+}
+
+using namespace NWNXLib::Services;
+
+namespace DotNET {
+
+static hostfxr_initialize_for_runtime_config_fn hostfxr_initialize_for_runtime_config;
+static hostfxr_get_runtime_delegate_fn          hostfxr_get_runtime_delegate;
+static hostfxr_close_fn                         hostfxr_close;
+
+bool DotNET::InitThunks()
+{
+    void *nethost = nullptr;
+    void *hostfxr = nullptr;
+
+    if (auto nethost_path = g_plugin->GetServices()->m_config->Get<std::string>("NETHOST_PATH"))
+    {
+        nethost = dlopen(nethost_path->c_str(), RTLD_LAZY);
+        ASSERT_MSG(nethost, "NETHOST_PATH specified ('%s') but failed to open libnethost.so at that path", *nethost_path);
+    }
+
+    if (!nethost)
+    {
+        const char *paths[] = {
+            "libnethost.so",
+            "./libnethost.so",
+            "lib/libnethost.so",
+            "/usr/share/dotnet/packs/Microsoft.NETCore.App.Host.linux-x64/3.0.0/runtimes/linux-x64/native/libnethost.so"
+        };
+        for (size_t i = 0; i < std::size(paths); i++)
+        {
+            nethost = dlopen(paths[i], RTLD_LAZY);
+            if (nethost)
+            {
+                LOG_INFO("Loaded libnethost.so from: %s (autodetected)", paths[i]);
+                break;
+            }
+        }
+    }
+
+    if (!nethost)
+    {
+        LOG_ERROR("Unable to load libnethost.so. .NET plugin will be unavailable.");
+        LOG_ERROR("If you're not using the .NET plugin, you can disable this message with 'NWNX_DOTNET_SKIP=y'");
+        return false;
+    }
+
+    auto get_hostfxr_path = (int(*)(char*,size_t*,const void*))dlsym(nethost, "get_hostfxr_path");
+    ASSERT_OR_RETURN(get_hostfxr_path != nullptr, false);
+
+    char buffer[PATH_MAX];
+    size_t buffer_size = PATH_MAX;
+    ASSERT_OR_RETURN(get_hostfxr_path(buffer, &buffer_size, nullptr) == 0, false);
+    dlclose(nethost);
+
+    hostfxr = dlopen(buffer, RTLD_LAZY);
+    ASSERT_OR_RETURN(hostfxr != nullptr, false);
+
+    hostfxr_initialize_for_runtime_config = (hostfxr_initialize_for_runtime_config_fn)dlsym(hostfxr, "hostfxr_initialize_for_runtime_config");
+    hostfxr_get_runtime_delegate          = (hostfxr_get_runtime_delegate_fn)         dlsym(hostfxr, "hostfxr_get_runtime_delegate");
+    hostfxr_close                         = (hostfxr_close_fn)                        dlsym(hostfxr, "hostfxr_close");
+
+    ASSERT_OR_RETURN(hostfxr_initialize_for_runtime_config != nullptr, false);
+    ASSERT_OR_RETURN(hostfxr_get_runtime_delegate != nullptr, false);
+    ASSERT_OR_RETURN(hostfxr_close != nullptr, false);
+
+    return true;
+}
+
+DotNET::DotNET(const Plugin::CreateParams& params) : Plugin(params)
+{
+    Instance = g_plugin = this;
+
+    // If the initial lib loads failed, we probably don't have .NET installed.
+    if (!InitThunks())
+        return;
+
+    auto assembly   = GetServices()->m_config->Require<std::string>("ASSEMBLY");
+    auto entrypoint = GetServices()->m_config->Get<std::string>("ENTRYPOINT", "NWN.EntryPoint, DotNetLib");
+
+    // Load .NET Core
+    hostfxr_handle cxt = nullptr;
+    auto runtimeconfig = assembly + ".runtimeconfig.json";
+    int rc = hostfxr_initialize_for_runtime_config(runtimeconfig.c_str(), nullptr, &cxt);
+    if (rc != 0 || cxt == nullptr)
+        LOG_FATAL("Unable to load runtime config '%s'; rc=0x%x", runtimeconfig, rc);
+
+    // Get the load assembly function pointer
+    load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer = nullptr;
+    rc = hostfxr_get_runtime_delegate(cxt, hdt_load_assembly_and_get_function_pointer, (void**)&load_assembly_and_get_function_pointer);
+    if (rc != 0 || load_assembly_and_get_function_pointer == nullptr)
+        LOG_FATAL("Unable to get load_assembly_and_get_function_pointer; rc=0x%x", rc);
+
+    hostfxr_close(cxt);
+
+
+    component_entry_point_fn bootstrap = nullptr;
+    auto dll = assembly + ".dll";
+    rc = load_assembly_and_get_function_pointer(dll.c_str(), entrypoint.c_str(),
+                                                "Bootstrap", nullptr, nullptr, (void**)&bootstrap);
+    if (rc != 0 || bootstrap == nullptr)
+        LOG_FATAL("Unable to get %s.Bootstrap() function: dll='%s'; rc=0x%x", entrypoint, dll, rc);
+
+    //
+    // Fill the function table to hand over to managed code
+    // NOTE: Only add new entries to the end of this table, DO NOT RESHUFFLE.
+    //
+    std::vector<void*> args;
+        args.push_back((void*)&GetFunctionPointer);
+        args.push_back((void*)&RegisterHandlers);
+        args.push_back((void*)&CallBuiltIn);
+        args.push_back((void*)&StackPushInteger);
+        args.push_back((void*)&StackPushFloat);
+        args.push_back((void*)&StackPushString);
+        args.push_back(nullptr); // reserved
+        args.push_back((void*)&StackPushObject);
+        args.push_back((void*)&StackPushVector);
+        args.push_back((void*)&StackPushEffect);
+        args.push_back((void*)&StackPushEvent);
+        args.push_back((void*)&StackPushLocation);
+        args.push_back((void*)&StackPushTalent);
+        args.push_back((void*)&StackPushItemProperty);
+        args.push_back((void*)&StackPopInteger);
+        args.push_back((void*)&StackPopFloat);
+        args.push_back((void*)&StackPopString);
+        args.push_back(nullptr); // reserved
+        args.push_back((void*)&StackPopObject);
+        args.push_back((void*)&StackPopVector);
+        args.push_back((void*)&StackPopEffect);
+        args.push_back((void*)&StackPopEvent);
+        args.push_back((void*)&StackPopLocation);
+        args.push_back((void*)&StackPopTalent);
+        args.push_back((void*)&StackPopItemProperty);
+        args.push_back((void*)&FreeEffect);
+        args.push_back((void*)&FreeEvent);
+        args.push_back((void*)&FreeLocation);
+        args.push_back((void*)&FreeTalent);
+        args.push_back((void*)&FreeItemProperty);
+        args.push_back((void*)&BeginClosure);
+        args.push_back((void*)&ClosureAssignCommand);
+        args.push_back((void*)&ClosureDelayCommand);
+        args.push_back((void*)&ClosureActionDoCommand);
+        args.push_back((void*)&nwnxSetFunction);
+        args.push_back((void*)&nwnxPushInt);
+        args.push_back((void*)&nwnxPushFloat);
+        args.push_back((void*)&nwnxPushObject);
+        args.push_back((void*)&nwnxPushString);
+        args.push_back(nullptr); // reserved
+        args.push_back((void*)&nwnxPushEffect);
+        args.push_back((void*)&nwnxPushItemProperty);
+        args.push_back((void*)&nwnxPopInt);
+        args.push_back((void*)&nwnxPopFloat);
+        args.push_back((void*)&nwnxPopObject);
+        args.push_back((void*)&nwnxPopString);
+        args.push_back(nullptr); // reserved
+        args.push_back((void*)&nwnxPopEffect);
+        args.push_back((void*)&nwnxPopItemProperty);
+        args.push_back((void*)&nwnxCallFunction);
+    rc = bootstrap(args.data(), args.size()*sizeof(void*));
+    if (rc != 0)
+        LOG_FATAL("Failed to execute bootstrap function; rc=0x%x", rc);
+
+    LOG_INFO("Managed code bootstrapped.");
+}
+
+DotNET::~DotNET() { }
+
+
+uintptr_t DotNET::GetFunctionPointer(const char *name)
+{
+    void *core = dlopen("NWNX_Core.so", RTLD_LAZY);
+    if (!core)
+    {
+        LOG_ERROR("Failed to open core handle: %s", dlerror());
+        return 0;
+    }
+    auto ret = reinterpret_cast<uintptr_t>(dlsym(core, name));
+    if (ret == 0)
+        LOG_WARNING("Failed to get symbol name '%s': %s", name, dlerror());
+    dlclose(core);
+    return ret;
+}
+
+
+static inline CVirtualMachine* GetVm()
+{
+    return Globals::VirtualMachine();
+}
+static inline CNWVirtualMachineCommands* GetVmCommands()
+{
+    return static_cast<CNWVirtualMachineCommands*>(GetVm()->m_pCmdImplementer);
+}
+
+
+void DotNET::RegisterHandlers(AllHandlers *handlers, unsigned size)
+{
+    if (size > sizeof(*handlers))
+    {
+        LOG_ERROR("RegisterHandlers argument contains too many entries, aborting");
+        return;
+    }
+    if (size < sizeof(*handlers))
+    {
+        LOG_WARNING("RegisterHandlers argument missing some entries - Managed/Unmanaged mismatch, update managed code");
+    }
+
+    LOG_INFO("Registering managed code handlers.");
+    Handlers = *handlers;
+
+    LOG_DEBUG("Registered main loop handler: %p", Handlers.MainLoopHandler);
+    g_plugin->GetServices()->m_hooks->RequestSharedHook<Functions::_ZN21CServerExoAppInternal8MainLoopEv, int32_t>(
+        +[](Services::Hooks::CallType type, CServerExoAppInternal*)
+        {
+            if (type == Services::Hooks::CallType::BEFORE_ORIGINAL)
+            {
+                static uint64_t frame = 0;
+                if (Handlers.MainLoopHandler)
+                {
+                    Globals::VirtualMachine()->m_nRecursionLevel++;
+                    Handlers.MainLoopHandler(frame);
+                    Globals::VirtualMachine()->m_nRecursionLevel--;
+                }
+                ++frame;
+            }
+        }
+    );
+
+
+    LOG_DEBUG("Registered runscript handler: %p", Handlers.RunScriptHandler);
+    static Hooking::FunctionHook* RunScriptHook;
+    g_plugin->GetServices()->m_hooks->RequestExclusiveHook<Functions::_ZN15CVirtualMachine9RunScriptEP10CExoStringji, int32_t>(
+        +[](CVirtualMachine* thisPtr, CExoString* script, Types::ObjectID objId, int32_t valid)
+        {
+            auto vm = Globals::VirtualMachine();
+            auto cmd = static_cast<CNWVirtualMachineCommands*>(vm->m_pCmdImplementer);
+
+            if (vm->m_nRecursionLevel++ == -1)
+            {
+                vm->m_cRunTimeStack.InitializeStack();
+                vm->m_cRunTimeStack.m_pVMachine = vm;
+            }
+
+            vm->m_oidObjectRunScript[vm->m_nRecursionLevel]    = objId;
+            vm->m_bValidObjectRunScript[vm->m_nRecursionLevel] = !!valid;
+            cmd->m_oidObjectRunScript    = vm->m_oidObjectRunScript[vm->m_nRecursionLevel];
+            cmd->m_bValidObjectRunScript = vm->m_bValidObjectRunScript[vm->m_nRecursionLevel];
+
+            LOG_DEBUG("Calling managed RunScriptHandler for script '%s' on Object 0x%08x", script->CStr(), objId);
+            int spBefore = vm->m_cRunTimeStack.GetStackPointer();
+            int32_t retval = Handlers.RunScriptHandler(script->CStr(), objId);
+            int spAfter = vm->m_cRunTimeStack.GetStackPointer();
+            ASSERT_MSG(spBefore == spAfter, "spBefore=%x, spAfter=%x", spBefore, spAfter);
+
+            if (--vm->m_nRecursionLevel != -1)
+            {
+                cmd->m_oidObjectRunScript = vm->m_oidObjectRunScript[vm->m_nRecursionLevel];
+                cmd->m_bValidObjectRunScript = vm->m_bValidObjectRunScript[vm->m_nRecursionLevel];
+            }
+
+            bool handled = retval != ~0;
+            if (handled)
+            {
+                vm->m_nReturnValueParameterType = 0x03;
+                vm->m_pReturnValue = reinterpret_cast<void*>(retval);
+            }
+            return handled ? true : RunScriptHook->CallOriginal<int32_t>(thisPtr, script, objId, valid);
+        }
+    );
+    RunScriptHook = g_plugin->GetServices()->m_hooks->FindHookByAddress(Functions::_ZN21CServerExoAppInternal8MainLoopEv);
+
+}
+
+}
---
+++ Plugins/DotNET/DotNET.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "Plugin.hpp"
+#include "API/Constants.hpp"
+#include "API/Types.hpp"
+#include "API/ALL_CLASSES.hpp"
+
+namespace DotNET {
+
+class DotNET : public NWNXLib::Plugin
+{
+public:
+    DotNET(const Plugin::CreateParams& params);
+    virtual ~DotNET();
+
+private:
+    static inline DotNET* Instance;
+
+    static bool InitThunks();
+
+    static inline char ReturnBuffer[256*1024];
+
+    // Bootstrap functions
+    using MainLoopHandlerType  = void (*)(uint64_t);
+    using RunScriptHandlerType = int (*)(const char *, uint32_t);
+
+    struct AllHandlers
+    {
+        MainLoopHandlerType  MainLoopHandler;
+        RunScriptHandlerType RunScriptHandler;
+    };
+    static inline AllHandlers Handlers;
+
+    static uintptr_t GetFunctionPointer(const char *name);
+    static void RegisterHandlers(AllHandlers *handlers, unsigned size);
+
+    // NWScript VM functions
+    static inline uint32_t PushedCount = 0;
+    static void CallBuiltIn(int32_t id);
+    static void StackPushInteger(int32_t value);
+    static void StackPushFloat(float value);
+    static void StackPushString(const char* value);
+    static void StackPushObject(uint32_t value);
+    static void StackPushVector(Vector value);
+    static void StackPushEffect(CGameEffect* value);
+    static void StackPushEvent(CScriptEvent* value);
+    static void StackPushLocation(CScriptLocation* value);
+    static void StackPushTalent(CScriptTalent* value);
+    static void StackPushItemProperty(CGameEffect* value);
+    static int32_t StackPopInteger();
+    static float StackPopFloat();
+    static const char* StackPopString();
+    static uint32_t StackPopObject();
+    static Vector StackPopVector();
+    static CGameEffect* StackPopEffect();
+    static CScriptEvent* StackPopEvent();
+    static CScriptLocation* StackPopLocation();
+    static CScriptTalent* StackPopTalent();
+    static CGameEffect* StackPopItemProperty();
+    static void FreeEffect(void* ptr);
+    static void FreeEvent(void* ptr);
+    static void FreeLocation(void* ptr);
+    static void FreeTalent(void* ptr);
+    static void FreeItemProperty(void* ptr);
+    static void BeginClosure(uint32_t value);
+    static int32_t ClosureAssignCommand(uint32_t oid, uint64_t eventId);
+    static int32_t ClosureDelayCommand(uint32_t oid, float duration, uint64_t eventId);
+    static int32_t ClosureActionDoCommand(uint32_t oid, uint64_t eventId);
+
+    // NWNX interface functions
+    static inline std::string nwnxActivePlugin;
+    static inline std::string nwnxActiveFunction;
+    static void nwnxSetFunction(const char *plugin, const char *function);
+    static void nwnxPushInt(int32_t n);
+    static void nwnxPushFloat(float f);
+    static void nwnxPushObject(uint32_t o);
+    static void nwnxPushString(const char *s);
+    static void nwnxPushEffect(CGameEffect *e);
+    static void nwnxPushItemProperty(CGameEffect *ip);
+    static int32_t nwnxPopInt();
+    static float nwnxPopFloat();
+    static uint32_t nwnxPopObject();
+    static const char* nwnxPopString();
+    static CGameEffect* nwnxPopEffect();
+    static CGameEffect* nwnxPopItemProperty();
+    static void nwnxCallFunction();
+};
+
+
+}
