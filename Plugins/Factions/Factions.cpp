586e13669832a77a679cd78d296bc0a38cd9c1f5
Jonathan Rolleston <jonathan.rolleston@paconsulting.com>
2020-02-13 15:45:38 +0000

    Try this one

--- Plugins/Factions/Factions.cpp
+++ Plugins/Factions/Factions.cpp
@@ -76,21 +76,20 @@ ArgumentStack Factions::SetFaction(ArgumentStack&& args)
 {
     int32_t retVal = -1;
 
-    if (auto *pObject = Object(args))
-    {
-        
-		  
-		  const auto nFacId = Services::Events::ExtractArgument<int32_t>(args);
-		  auto* pFactionManager= Globals::AppManager()->m_pServerExoApp->m_pcExoAppInternal->m_pFactionManager;
-		  auto* pFaction = pFactionManager->GetFaction(nFacId);
-		  if (pFaction){
-		      pFaction->AddMember(pObject);
-			  retVal = 1;
-		  }
-
-        
-    }
-
+	const auto objectId = Services::Events::ExtractArgument<Types::ObjectID>(args);
+	if (objectId == Constants::OBJECT_INVALID)
+	    {
+	        LOG_NOTICE("NWNX_Faction SetFaction called on OBJECT_INVALID");
+	    }else if (auto *pGameObject = Globals::AppManager()->m_pServerExoApp->GetGameObject(objectId))
+        {
+  		  const auto nFacId = Services::Events::ExtractArgument<int32_t>(args);
+  		  auto* pFactionManager= Globals::AppManager()->m_pServerExoApp->m_pcExoAppInternal->m_pFactionManager->GetFaction(nFacId);
+  		  if (pFaction){
+  		      pFaction->AddMember(pObject);
+  			  retVal = 1;
+  		  }
+			
+        }
     return Services::Events::Arguments(retVal);
 }
 
