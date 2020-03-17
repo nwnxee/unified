856c2782e38abd558537f9289ba7455d05038bd8
ci-build <ci-build@nwnx.io>
2020-03-17 19:55:56 +0000

    [skip ci] Updates

--- dark/dir_ce92b3ebeae221968cfbe8aae0d60ffe.js
+++ dark/dir_ce92b3ebeae221968cfbe8aae0d60ffe.js
@@ -1,127 +1,127 @@
 var dir_ce92b3ebeae221968cfbe8aae0d60ffe =
 [
-    [ "Environment Variables", "group__nwnx.html#autotoc_md58", null ],
-    [ "Console Commands", "group__nwnx.html#autotoc_md59", null ],
-    [ "Plugin Management", "group__nwnx.html#autotoc_md60", [
-      [ "Environment Variable Configuration", "group__nwnx.html#autotoc_md61", null ],
-      [ "Plugin Loading", "group__nwnx.html#autotoc_md62", null ],
-      [ "Plugin Log Level", "group__nwnx.html#autotoc_md63", null ]
+    [ "Environment Variables", "group__nwnx.html#autotoc_md66", null ],
+    [ "Console Commands", "group__nwnx.html#autotoc_md67", null ],
+    [ "Plugin Management", "group__nwnx.html#autotoc_md68", [
+      [ "Environment Variable Configuration", "group__nwnx.html#autotoc_md69", null ],
+      [ "Plugin Loading", "group__nwnx.html#autotoc_md70", null ],
+      [ "Plugin Log Level", "group__nwnx.html#autotoc_md71", null ]
     ] ],
-    [ "Core Functions For Plugin Developers", "group__nwnx.html#autotoc_md64", null ],
-    [ "Environment Variables", "group__chat.html#autotoc_md75", null ],
-    [ "Example", "group__damage.html#autotoc_md76", null ],
-    [ "Environment Variables", "group__dotnet.html#autotoc_md77", null ],
-    [ "Basic Setup", "group__dotnet.html#autotoc_md78", [
-      [ "Dependencies", "group__dotnet.html#autotoc_md79", null ],
-      [ "Compile managed code", "group__dotnet.html#autotoc_md80", null ],
-      [ "Run NWNX with your DLL", "group__dotnet.html#autotoc_md81", null ],
-      [ "Troubleshooting", "group__dotnet.html#autotoc_md82", [
-        [ "Sample", "group__nwnx.html#autotoc_md65", null ],
-        [ "libnethost.so", "group__dotnet.html#autotoc_md83", null ],
-        [ "runtime config", "group__dotnet.html#autotoc_md84", null ],
-        [ "Bootstrap()", "group__dotnet.html#autotoc_md85", null ]
+    [ "Core Functions For Plugin Developers", "group__nwnx.html#autotoc_md72", null ],
+    [ "Environment Variables", "group__chat.html#autotoc_md83", null ],
+    [ "Example", "group__damage.html#autotoc_md84", null ],
+    [ "Environment Variables", "group__dotnet.html#autotoc_md85", null ],
+    [ "Basic Setup", "group__dotnet.html#autotoc_md86", [
+      [ "Dependencies", "group__dotnet.html#autotoc_md87", null ],
+      [ "Compile managed code", "group__dotnet.html#autotoc_md88", null ],
+      [ "Run NWNX with your DLL", "group__dotnet.html#autotoc_md89", null ],
+      [ "Troubleshooting", "group__dotnet.html#autotoc_md90", [
+        [ "Sample", "group__nwnx.html#autotoc_md73", null ],
+        [ "libnethost.so", "group__dotnet.html#autotoc_md91", null ],
+        [ "runtime config", "group__dotnet.html#autotoc_md92", null ],
+        [ "Bootstrap()", "group__dotnet.html#autotoc_md93", null ]
       ] ]
     ] ],
-    [ "How it works (basic)", "group__dotnet.html#autotoc_md86", [
-      [ "<tt>OnStart</tt>", "group__dotnet.html#autotoc_md87", null ],
-      [ "<tt>OnRunScript</tt>", "group__dotnet.html#autotoc_md88", null ],
-      [ "<tt>OnMainLoop</tt>", "group__dotnet.html#autotoc_md89", null ],
-      [ "Using NWScript in C", "group__dotnet.html#autotoc_md90", null ]
+    [ "How it works (basic)", "group__dotnet.html#autotoc_md94", [
+      [ "<tt>OnStart</tt>", "group__dotnet.html#autotoc_md95", null ],
+      [ "<tt>OnRunScript</tt>", "group__dotnet.html#autotoc_md96", null ],
+      [ "<tt>OnMainLoop</tt>", "group__dotnet.html#autotoc_md97", null ],
+      [ "Using NWScript in C", "group__dotnet.html#autotoc_md98", null ]
     ] ],
-    [ "Advanced stuff", "group__dotnet.html#autotoc_md91", [
-      [ "NWN types", "group__dotnet.html#autotoc_md92", null ],
-      [ "Bootstrapping", "group__dotnet.html#autotoc_md93", null ],
-      [ "OBJECT_SELF", "group__dotnet.html#autotoc_md94", null ],
-      [ "Closures", "group__dotnet.html#autotoc_md95", null ],
-      [ "Exceptions", "group__dotnet.html#autotoc_md96", null ]
+    [ "Advanced stuff", "group__dotnet.html#autotoc_md99", [
+      [ "NWN types", "group__dotnet.html#autotoc_md100", null ],
+      [ "Bootstrapping", "group__dotnet.html#autotoc_md101", null ],
+      [ "OBJECT_SELF", "group__dotnet.html#autotoc_md102", null ],
+      [ "Closures", "group__dotnet.html#autotoc_md103", null ],
+      [ "Exceptions", "group__dotnet.html#autotoc_md104", null ]
     ] ],
-    [ "Notes", "group__elc.html#autotoc_md97", null ],
-    [ "Environment Variables", "group__elc.html#autotoc_md98", null ],
-    [ "Example Script", "group__elc.html#autotoc_md99", null ],
-    [ "Notes", "group__experimental.html#autotoc_md161", null ],
-    [ "Environment Variables", "group__experimental.html#autotoc_md162", null ],
-    [ "Environment Variables", "group__lua.html#autotoc_md163", null ],
-    [ "Quick start", "group__lua.html#autotoc_md164", [
-      [ "Docker", "group__lua.html#autotoc_md165", null ],
-      [ "Native", "group__lua.html#autotoc_md166", null ]
+    [ "Notes", "group__elc.html#autotoc_md105", null ],
+    [ "Environment Variables", "group__elc.html#autotoc_md106", null ],
+    [ "Example Script", "group__elc.html#autotoc_md107", null ],
+    [ "Notes", "group__experimental.html#autotoc_md169", null ],
+    [ "Environment Variables", "group__experimental.html#autotoc_md170", null ],
+    [ "Environment Variables", "group__lua.html#autotoc_md171", null ],
+    [ "Quick start", "group__lua.html#autotoc_md172", [
+      [ "Docker", "group__lua.html#autotoc_md173", null ],
+      [ "Native", "group__lua.html#autotoc_md174", null ]
     ] ],
-    [ "Documentation", "group__lua.html#autotoc_md167", null ],
-    [ "Installation and configuration", "group__lua.html#autotoc_md168", null ],
-    [ "Notes on Compilation", "group__lua.html#autotoc_md169", null ],
-    [ "Note on LuaJIT and extensions", "group__lua.html#autotoc_md170", null ],
-    [ "Environment Variables", "group__maxlevel.html#autotoc_md171", null ],
-    [ "Required Plugins", "group__maxlevel.html#autotoc_md172", null ],
-    [ "Issues", "group__maxlevel.html#autotoc_md173", null ],
-    [ "Setup", "group__maxlevel.html#autotoc_md174", null ],
-    [ "InfluxDB setup", "group__metrics__influxdb.html#autotoc_md175", null ],
-    [ "Environment Variables", "group__metrics__influxdb.html#autotoc_md176", null ],
-    [ "Environment Variables", "group__optimizations.html#autotoc_md177", null ],
-    [ "Function Documentation and Examples", "group__player.html#autotoc_md178", null ],
-    [ "Environment Variables", "group__profiler.html#autotoc_md180", null ],
-    [ "Environmental Variables", "group__race.html#autotoc_md181", null ],
-    [ "Required NWNX Plugins", "group__race.html#autotoc_md182", null ],
-    [ "Setup", "group__race.html#autotoc_md183", [
-      [ "The 2da Method", "group__race.html#autotoc_md184", null ],
-      [ "The Scripting Method", "group__race.html#autotoc_md185", null ]
+    [ "Documentation", "group__lua.html#autotoc_md175", null ],
+    [ "Installation and configuration", "group__lua.html#autotoc_md176", null ],
+    [ "Notes on Compilation", "group__lua.html#autotoc_md177", null ],
+    [ "Note on LuaJIT and extensions", "group__lua.html#autotoc_md178", null ],
+    [ "Environment Variables", "group__maxlevel.html#autotoc_md179", null ],
+    [ "Required Plugins", "group__maxlevel.html#autotoc_md180", null ],
+    [ "Issues", "group__maxlevel.html#autotoc_md181", null ],
+    [ "Setup", "group__maxlevel.html#autotoc_md182", null ],
+    [ "InfluxDB setup", "group__metrics__influxdb.html#autotoc_md183", null ],
+    [ "Environment Variables", "group__metrics__influxdb.html#autotoc_md184", null ],
+    [ "Environment Variables", "group__optimizations.html#autotoc_md185", null ],
+    [ "Function Documentation and Examples", "group__player.html#autotoc_md186", null ],
+    [ "Environment Variables", "group__profiler.html#autotoc_md188", null ],
+    [ "Environmental Variables", "group__race.html#autotoc_md189", null ],
+    [ "Required NWNX Plugins", "group__race.html#autotoc_md190", null ],
+    [ "Setup", "group__race.html#autotoc_md191", [
+      [ "The 2da Method", "group__race.html#autotoc_md192", null ],
+      [ "The Scripting Method", "group__race.html#autotoc_md193", null ]
     ] ],
-    [ "Racial Modifiers", "group__race.html#autotoc_md186", [
-      [ "AB", "group__race.html#autotoc_md187", null ],
-      [ "ABVSRACE", "group__race.html#autotoc_md190", null ],
-      [ "AC", "group__race.html#autotoc_md193", null ],
-      [ "ACVSRACE", "group__race.html#autotoc_md196", null ],
-      [ "CONCEALMENT", "group__race.html#autotoc_md199", null ],
-      [ "DMGREDUCTION", "group__race.html#autotoc_md202", null ],
-      [ "DMGRESIST", "group__race.html#autotoc_md205", null ],
-      [ "DMGIMMUNITY", "group__race.html#autotoc_md208", null ],
-      [ "FEAT", "group__race.html#autotoc_md211", null ],
-      [ "FEATUSAGE", "group__race.html#autotoc_md214", null ],
-      [ "IMMUNITY", "group__race.html#autotoc_md217", null ],
-      [ "INITIATIVE", "group__race.html#autotoc_md220", null ],
-      [ "MOVEMENTSPEED", "group__race.html#autotoc_md223", null ],
-      [ "RACE", "group__race.html#autotoc_md226", null ],
-      [ "REGENERATION", "group__race.html#autotoc_md229", null ],
-      [ "SAVE", "group__race.html#autotoc_md232", null ],
-      [ "SAVEVSRACE", "group__race.html#autotoc_md235", null ],
-      [ "SAVEVSTYPE", "group__race.html#autotoc_md238", null ],
-      [ "SKILL", "group__race.html#autotoc_md241", null ],
-      [ "SPELLIMMUNITY", "group__race.html#autotoc_md244", null ],
-      [ "SRCHARGEN", "group__race.html#autotoc_md247", null ],
-      [ "SRINCLEVEL", "group__race.html#autotoc_md250", null ]
+    [ "Racial Modifiers", "group__race.html#autotoc_md194", [
+      [ "AB", "group__race.html#autotoc_md195", null ],
+      [ "ABVSRACE", "group__race.html#autotoc_md198", null ],
+      [ "AC", "group__race.html#autotoc_md201", null ],
+      [ "ACVSRACE", "group__race.html#autotoc_md204", null ],
+      [ "CONCEALMENT", "group__race.html#autotoc_md207", null ],
+      [ "DMGREDUCTION", "group__race.html#autotoc_md210", null ],
+      [ "DMGRESIST", "group__race.html#autotoc_md213", null ],
+      [ "DMGIMMUNITY", "group__race.html#autotoc_md216", null ],
+      [ "FEAT", "group__race.html#autotoc_md219", null ],
+      [ "FEATUSAGE", "group__race.html#autotoc_md222", null ],
+      [ "IMMUNITY", "group__race.html#autotoc_md225", null ],
+      [ "INITIATIVE", "group__race.html#autotoc_md228", null ],
+      [ "MOVEMENTSPEED", "group__race.html#autotoc_md231", null ],
+      [ "RACE", "group__race.html#autotoc_md234", null ],
+      [ "REGENERATION", "group__race.html#autotoc_md237", null ],
+      [ "SAVE", "group__race.html#autotoc_md240", null ],
+      [ "SAVEVSRACE", "group__race.html#autotoc_md243", null ],
+      [ "SAVEVSTYPE", "group__race.html#autotoc_md246", null ],
+      [ "SKILL", "group__race.html#autotoc_md249", null ],
+      [ "SPELLIMMUNITY", "group__race.html#autotoc_md252", null ],
+      [ "SRCHARGEN", "group__race.html#autotoc_md255", null ],
+      [ "SRINCLEVEL", "group__race.html#autotoc_md258", null ]
     ] ],
-    [ "Hints", "group__redis.html#autotoc_md253", null ],
-    [ "Setup", "group__redis.html#autotoc_md254", null ],
-    [ "Accessing Redis from nwscript", "group__redis.html#autotoc_md255", null ],
-    [ "Getting started with PubSub", "group__redis.html#autotoc_md256", [
-      [ "Moving on", "group__redis.html#autotoc_md257", null ]
+    [ "Hints", "group__redis.html#autotoc_md261", null ],
+    [ "Setup", "group__redis.html#autotoc_md262", null ],
+    [ "Accessing Redis from nwscript", "group__redis.html#autotoc_md263", null ],
+    [ "Getting started with PubSub", "group__redis.html#autotoc_md264", [
+      [ "Moving on", "group__redis.html#autotoc_md265", null ]
     ] ],
-    [ "Environment Variables", "group__redis.html#autotoc_md258", null ],
-    [ "Environment Variables", "group__rename.html#autotoc_md259", null ],
-    [ "Sample Include", "group__rename.html#autotoc_md260", null ],
-    [ "Environment Variables", "group__ruby.html#autotoc_md261", null ],
-    [ "Plugin Sample Usage", "group__skillranks.html#autotoc_md262", null ],
-    [ "How to install?", "group__spellchecker.html#autotoc_md269", null ],
-    [ "Environment Variables", "group__spellchecker.html#autotoc_md270", null ],
-    [ "Migration", "group__sql.html#autotoc_md274", null ],
-    [ "Environment Variables", "group__sql.html#autotoc_md275", [
-      [ "NWNX_SQL_TYPE", "group__sql.html#autotoc_md276", null ],
-      [ "NWNX_SQL_HOST", "group__sql.html#autotoc_md277", null ],
-      [ "NWNX_SQL_PORT", "group__sql.html#autotoc_md278", null ],
-      [ "NWNX_SQL_USERNAME", "group__sql.html#autotoc_md279", null ],
-      [ "NWNX_SQL_PASSWORD", "group__sql.html#autotoc_md280", null ],
-      [ "NWNX_SQL_DATABASE", "group__sql.html#autotoc_md281", null ],
-      [ "NWNX_SQL_QUERY_METRICS", "group__sql.html#autotoc_md282", null ],
-      [ "NWNX_SQL_USE_UTF8", "group__sql.html#autotoc_md283", null ],
-      [ "NWNX_SQL_CHARACTER_SET", "group__sql.html#autotoc_md284", null ]
+    [ "Environment Variables", "group__redis.html#autotoc_md266", null ],
+    [ "Environment Variables", "group__rename.html#autotoc_md267", null ],
+    [ "Sample Include", "group__rename.html#autotoc_md268", null ],
+    [ "Environment Variables", "group__ruby.html#autotoc_md269", null ],
+    [ "Plugin Sample Usage", "group__skillranks.html#autotoc_md270", null ],
+    [ "How to install?", "group__spellchecker.html#autotoc_md277", null ],
+    [ "Environment Variables", "group__spellchecker.html#autotoc_md278", null ],
+    [ "Migration", "group__sql.html#autotoc_md282", null ],
+    [ "Environment Variables", "group__sql.html#autotoc_md283", [
+      [ "NWNX_SQL_TYPE", "group__sql.html#autotoc_md284", null ],
+      [ "NWNX_SQL_HOST", "group__sql.html#autotoc_md285", null ],
+      [ "NWNX_SQL_PORT", "group__sql.html#autotoc_md286", null ],
+      [ "NWNX_SQL_USERNAME", "group__sql.html#autotoc_md287", null ],
+      [ "NWNX_SQL_PASSWORD", "group__sql.html#autotoc_md288", null ],
+      [ "NWNX_SQL_DATABASE", "group__sql.html#autotoc_md289", null ],
+      [ "NWNX_SQL_QUERY_METRICS", "group__sql.html#autotoc_md290", null ],
+      [ "NWNX_SQL_USE_UTF8", "group__sql.html#autotoc_md291", null ],
+      [ "NWNX_SQL_CHARACTER_SET", "group__sql.html#autotoc_md292", null ]
     ] ],
-    [ "Environment Variables", "group__threadwatchdog.html#autotoc_md285", null ],
-    [ "Environment Variables", "group__tracking.html#autotoc_md286", null ],
-    [ "Environment Variables", "group__tweaks.html#autotoc_md287", null ],
-    [ "Environment variable values", "group__tweaks.html#autotoc_md288", [
-      [ "NWNX_TWEAKS_ITEM_CHARGES_COST_MODE", "group__tweaks.html#autotoc_md289", null ]
+    [ "Environment Variables", "group__threadwatchdog.html#autotoc_md293", null ],
+    [ "Environment Variables", "group__tracking.html#autotoc_md294", null ],
+    [ "Environment Variables", "group__tweaks.html#autotoc_md295", null ],
+    [ "Environment variable values", "group__tweaks.html#autotoc_md296", [
+      [ "NWNX_TWEAKS_ITEM_CHARGES_COST_MODE", "group__tweaks.html#autotoc_md297", null ]
     ] ],
-    [ "Environment Variables", "group__util.html#autotoc_md290", null ],
-    [ "Limitations", "group__webhook.html#autotoc_md296", null ],
-    [ "Example usage", "group__webhook.html#autotoc_md297", null ],
+    [ "Environment Variables", "group__util.html#autotoc_md298", null ],
+    [ "Limitations", "group__webhook.html#autotoc_md304", null ],
+    [ "Example usage", "group__webhook.html#autotoc_md305", null ],
     [ "nwnx.nss", "nwnx_8nss.html", "nwnx_8nss" ],
     [ "nwnx_consts.nss", "nwnx__consts_8nss.html", "nwnx__consts_8nss" ],
     [ "nwnx_tests.nss", "nwnx__tests_8nss.html", "nwnx__tests_8nss" ]
