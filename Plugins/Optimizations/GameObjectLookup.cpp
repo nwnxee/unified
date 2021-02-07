89ae2aaf37af7111e339fafc1f6c06c0b4cba346
mtijanic <sherincall@gmail.com>
2021-02-07 13:05:16 +0100

    optimizations: Abstract GOA hash table for reuse

--- Plugins/Optimizations/GameObjectLookup.cpp
+++ Plugins/Optimizations/GameObjectLookup.cpp
@@ -1,5 +1,7 @@
 #include "nwnx.hpp"
 
+#include "HashTable32.hpp"
+
 #include "API/Functions.hpp"
 #include "API/Constants.hpp"
 #include "API/CAppManager.hpp"
@@ -25,35 +27,13 @@ namespace Optimizations {
 using namespace NWNXLib;
 using namespace NWNXLib::API;
 
-static constexpr uint32_t ObjectsInNode = 4;
-static constexpr uint32_t L0CacheSize = 4;
-
-struct ObjectNode
-{
-    CGameObject* m_objectPtr[ObjectsInNode];
-    uint32_t     m_objectId[ObjectsInNode];
-    ObjectNode*  m_nextNode;
-    uint32_t     m_count;
-    uint8_t      m_padding[4]; // For cache line alignment
-};
-static_assert(sizeof(ObjectNode) == 64);
-
-// DO NOT REARRANGE - optimal placement across cache lines
-static struct alignas(64)
-{
-    CGameObject*   m_pCacheObjects[L0CacheSize];
-    uint32_t       m_nCacheIds[L0CacheSize];
-    ObjectNode*    m_pArray;
-    uint32_t       m_nArraySize;
-    uint32_t       m_nNextObjectArrayID[2];
-    uint32_t       m_nNextCharArrayID[2];
-} goa;
+static HashTable32<CGameObject, 1024*1024> s_table;
+static uint32_t s_nNextObjectArrayID[2];
+static uint32_t s_nNextCharArrayID[2];
 
 enum { InternalObject = 0, ExternalObject = 1};
 enum { Success = 0, BadId = 1, NullGameObject = 4 };
 
-static void Initialize(uint32_t nLogGameObjectCache);
-static void Finalize();
 static uint32_t GetNextID(void*, BOOL bInternal, BOOL bCharacter);
 static uint8_t AddObjectAtPos(void*, uint32_t id, CGameObject *ptr);
 static uint8_t AddExternalObject(void*, uint32_t &id, CGameObject *ptr);
@@ -72,25 +52,11 @@ void GameObjectLookup()
 
     LOG_INFO("Using optimal CGameObjectArray implementation");
 
+    s_table.Initialize();
     static Hooks::Hook s_gameObjectArrayCtorHook = Hooks::HookFunction(API::Functions::_ZN16CGameObjectArrayC1Ei,
         (void*)+[](CGameObjectArray* pThis) -> void
         {
-            s_gameObjectArrayCtorHook->CallOriginal<void>(pThis);
-
-            // Reading the config is currently.. complicated in NWNX, so just
-            // let the CGOA constructor read it, and then undo the allocation.
-            delete[] pThis->m_pArray;
-            pThis->m_pArray = nullptr;
-            pThis->m_nArraySize = 0;
-            pThis->m_nGameObjectCache = 0;
-            Initialize(pThis->m_nLogGameObjectCache);
-        }, Hooks::Order::Final);
-
-    static Hooks::Hook s_gameObjectArrayDtorHook = Hooks::HookFunction(API::Functions::_ZN16CGameObjectArrayD1Ev,
-        (void*)+[](CGameObjectArray *pThis) -> void
-        {
-            s_gameObjectArrayDtorHook->CallOriginal<void>(pThis);
-            Finalize();
+            memset(pThis, 0, sizeof(*pThis));
         }, Hooks::Order::Final);
 
     static auto s_AddObjectAtPos    = Hooks::HookFunction(API::Functions::_ZN16CGameObjectArray14AddObjectAtPosEjP11CGameObject, (void*)&AddObjectAtPos, Hooks::Order::Final);
@@ -99,50 +65,19 @@ void GameObjectLookup()
     static auto s_Delete1           = Hooks::HookFunction(API::Functions::_ZN16CGameObjectArray6DeleteEjPP11CGameObject, (void*)&Delete, Hooks::Order::Final);
     static auto s_Delete2           = Hooks::HookFunction(API::Functions::_ZN16CGameObjectArray6DeleteEj, (void*)+[](void* p, uint32_t id) -> uint8_t { return Delete(p, id, nullptr); }, Hooks::Order::Final);
     static auto s_GetGameObject     = Hooks::HookFunction(API::Functions::_ZN16CGameObjectArray13GetGameObjectEjPP11CGameObject, (void*)&GetGameObject, Hooks::Order::Final);
-}
 
-
-static void Initialize(uint32_t nLogGameObjectCache)
-{
-    goa.m_nArraySize = (1 << nLogGameObjectCache);
-
-    // Make sure the allocation is properly aligned on a 4k page boundary
-    const size_t array_size = goa.m_nArraySize * sizeof(ObjectNode);
-    goa.m_pArray = (ObjectNode*)aligned_alloc(0x1000, array_size);
-    memset(goa.m_pArray, 0, array_size);
-
-    for (uint32_t i = 0; i < goa.m_nArraySize; i++)
-    {
-        for (uint32_t j = 0; j < ObjectsInNode; j++)
-            goa.m_pArray[i].m_objectId[j] = Constants::OBJECT_INVALID;
-    }
-
-    for (uint32_t i = 0; i < L0CacheSize; i++)
-        goa.m_nCacheIds[i] = Constants::OBJECT_INVALID;
-
-    goa.m_nNextObjectArrayID[InternalObject] = 0x00000000;
-    goa.m_nNextCharArrayID[InternalObject]   = 0x7FFFFFFF;
-    goa.m_nNextObjectArrayID[ExternalObject] = 0x80000000;
-    goa.m_nNextCharArrayID[ExternalObject]   = 0xFFFFFFFF;
+    s_nNextObjectArrayID[InternalObject] = 0x00000000;
+    s_nNextCharArrayID[InternalObject]   = 0x7FFFFFFF;
+    s_nNextObjectArrayID[ExternalObject] = 0x80000000;
+    s_nNextCharArrayID[ExternalObject]   = 0xFFFFFFFF;
 
     SetupWrapperHooks();
-}
-
-
-static void Finalize()
-{
-    for (uint32_t index = 0; index < goa.m_nArraySize; index++)
-    {
-        while (goa.m_pArray[index].m_objectPtr[0])
-            delete goa.m_pArray[index].m_objectPtr[0];
-    }
-    free(goa.m_pArray);
 }
 
 static uint32_t GetNextID(void*, BOOL bInternal, BOOL bCharacter)
 {
     int type = bInternal ? InternalObject : ExternalObject;
-    uint32_t ret = bCharacter ? goa.m_nNextCharArrayID[type]-- : goa.m_nNextObjectArrayID[type]++;
+    uint32_t ret = bCharacter ? s_nNextCharArrayID[type]-- : s_nNextObjectArrayID[type]++;
     SyncWithGameGOA();
     return ret;
 }
@@ -152,33 +87,8 @@ static uint8_t AddObjectAtPos(void*, uint32_t id, CGameObject *ptr)
     if (!ptr)
         return NullGameObject;
 
-    ObjectNode *node = &goa.m_pArray[id & (goa.m_nArraySize-1)];
-    while (true)
-    {
-        for (uint32_t i = 0; i < ObjectsInNode; i++)
-        {
-            if (node->m_objectId[i] == id)
-                return BadId;
-
-            if (node->m_objectPtr[i] == nullptr)
-            {
-                node->m_objectPtr[i] = ptr;
-                node->m_objectId[i] = id;
-                node->m_count++;
-                return Success;
-            }
-        }
-        if (node->m_nextNode == nullptr)
-        {
-            node->m_nextNode = new ObjectNode();
-            memset(node->m_nextNode, 0, sizeof(*node->m_nextNode));
-            for (uint32_t i = 0; i < ObjectsInNode; i++)
-                node->m_nextNode->m_objectId[i] = Constants::OBJECT_INVALID;
-        }
-        node = node->m_nextNode;
-    }
+    return s_table.Add(id, ptr) ? Success : BadId;
 }
-
 
 static uint8_t AddExternalObject(void*, uint32_t &id, CGameObject *ptr)
 {
@@ -188,166 +98,42 @@ static uint8_t AddExternalObject(void*, uint32_t &id, CGameObject *ptr)
     {
         if (id < 0xFF000001)
             return BadId;
-        goa.m_nNextCharArrayID[ExternalObject] = std::min(goa.m_nNextCharArrayID[ExternalObject], id-1);
+        s_nNextCharArrayID[ExternalObject] = std::min(s_nNextCharArrayID[ExternalObject], id-1);
     }
     else
     {
         if (id < 0x80000000 || id > 0xFEFFFFFF)
             return BadId;
-        goa.m_nNextObjectArrayID[ExternalObject] = std::max(goa.m_nNextObjectArrayID[ExternalObject], id+1);
+        s_nNextObjectArrayID[ExternalObject] = std::max(s_nNextObjectArrayID[ExternalObject], id+1);
     }
     SyncWithGameGOA();
     return AddObjectAtPos(nullptr, id, ptr);
 }
-
 
 static uint8_t AddInternalObject(void*, uint32_t &id, CGameObject *ptr, bool bCharacter)
 {
     return AddObjectAtPos(nullptr, (id = GetNextID(nullptr, true, bCharacter)), ptr);
 }
 
-
 static uint8_t Delete(void*, uint32_t id, CGameObject** ptr)
 {
-    if (id == Constants::OBJECT_INVALID)
-        return BadId;
-
-    // Clear the cache first
-    for (uint32_t i = 0; i < L0CacheSize; i++)
-    {
-        if (goa.m_nCacheIds[i] == id)
-            goa.m_nCacheIds[i] = Constants::OBJECT_INVALID;
-    }
-
-    ObjectNode *node = &goa.m_pArray[id & (goa.m_nArraySize-1)], *prev = nullptr;
-    while (node != nullptr)
-    {
-        for (uint32_t i = 0; i < ObjectsInNode; i++)
-        {
-            if (node->m_objectId[i] == id)
-            {
-                if (ptr) *ptr = node->m_objectPtr[i];
-                //
-                // Pack up the index every time we delete a node to keep optimal lookup perf
-                //
-                // 1. Removed last entry in a node
-                if (node->m_count == 1 && prev != nullptr) // never delete root node
-                {
-                    prev->m_nextNode = node->m_nextNode;
-                    delete node;
-                }
-                // 2. We have another node in the chain. Take the last node's last entry
-                else if (node->m_nextNode != nullptr)
-                {
-                    ObjectNode *last = node->m_nextNode, *secondlast = node;
-                    while (last->m_nextNode)
-                    {
-                        secondlast = last;
-                        last = last->m_nextNode;
-                    }
-                    last->m_count--;
-                    node->m_objectPtr[i] = last->m_objectPtr[last->m_count];
-                    node->m_objectId[i]  = last->m_objectId[last->m_count];
-                    if (last->m_count == 0)
-                    {
-                        secondlast->m_nextNode = nullptr;
-                        delete last;
-                    }
-                    else
-                    {
-                        last->m_objectPtr[last->m_count] = nullptr;
-                        last->m_objectId[last->m_count]  = Constants::OBJECT_INVALID;
-                    }
-                }
-                // 3. Removed an in the last chain link
-                else
-                {
-                    node->m_count--;
-                    node->m_objectPtr[i] = node->m_objectPtr[node->m_count];
-                    node->m_objectId[i]  = node->m_objectId[node->m_count];
-                    node->m_objectPtr[node->m_count] = nullptr;
-                    node->m_objectId[node->m_count] = Constants::OBJECT_INVALID;
-                }
-                return Success;
-            }
-        }
-        prev = node;
-        node = node->m_nextNode;
-    }
-    return BadId;
+    return s_table.Delete(id, ptr) ? Success : BadId;
 }
 
-#pragma GCC optimize ("unroll-loops")
-#define PREFETCH(a) __builtin_prefetch(a, 0, 0)
 static uint8_t GetGameObject(void*, uint32_t id, CGameObject** ptr)
 {
-    // This function needs to be AS FAST AS POSSIBLE, so be very careful what you add to it.
-    // The optimization should target the average case where the object does exist.
-    if (id != Constants::OBJECT_INVALID)
-    {
-        // We first check our first level cache for the mappings. However, while
-        // we're checking the logic there, we can fetch the required node from
-        // the main memory into the CPU L1 cache. If our own cache does not contain
-        // the mapping, we won't have to wait on a CPU cache miss later.
-        ObjectNode *node = &goa.m_pArray[id & (goa.m_nArraySize-1)];
-        PREFETCH(node);
-
-        // Check our first level cache first
-        for (uint32_t i = 0; i < L0CacheSize; i++)
-        {
-            if (goa.m_nCacheIds[i] == id)
-            {
-                *ptr = goa.m_pCacheObjects[i];
-                PREFETCH(*ptr);
-
-                // Shift both arrays to the right to make the 0th entry available
-                if (i != 0)
-                {
-                    memmove(&goa.m_nCacheIds[1],     &goa.m_nCacheIds[0],     i * sizeof(goa.m_nCacheIds[0]));
-                    memmove(&goa.m_pCacheObjects[1], &goa.m_pCacheObjects[0], i * sizeof(goa.m_pCacheObjects[0]));
-                    goa.m_nCacheIds[0] = id;
-                    goa.m_pCacheObjects[0] = *ptr;
-                }
-                return Success;
-            }
-        }
-
-        do
-        {
-            // Another PREFETCH(node->m_nextNode) here could help in cases where we're likely to have
-            // multiple chained nodes for this index. However, that's an edge case (just increase cache size!)
-            // and we're trying to optimize for the average case when the cache size is adequate.
-            for (uint32_t i = 0; i < ObjectsInNode; i++)
-            {
-                if (node->m_objectId[i] == id)
-                {
-                    *ptr = node->m_objectPtr[i];
-                    PREFETCH(*ptr);
-
-                    const uint32_t moveelems = L0CacheSize - 1;
-                    memmove(&goa.m_nCacheIds[1],     &goa.m_nCacheIds[0],     moveelems * sizeof(goa.m_nCacheIds[0]));
-                    memmove(&goa.m_pCacheObjects[1], &goa.m_pCacheObjects[0], moveelems * sizeof(goa.m_pCacheObjects[0]));
-                    goa.m_nCacheIds[0] = id;
-                    goa.m_pCacheObjects[0] = *ptr;
-                    return Success;
-                }
-            }
-            node = node->m_nextNode;
-        } while (node != nullptr);
-    }
-
-    *ptr = NULL;
-    return BadId;
+    *ptr = s_table.Get(id);
+    return *ptr ? Success : BadId;
 }
 
 static void SyncWithGameGOA()
 {
     if (auto* pGameObjectArray = Globals::AppManager()->m_pServerExoApp->GetObjectArray())
     {
-        pGameObjectArray->m_nNextObjectArrayID[0] = goa.m_nNextObjectArrayID[0];
-        pGameObjectArray->m_nNextObjectArrayID[1] = goa.m_nNextObjectArrayID[1];
-        pGameObjectArray->m_nNextCharArrayID[0] = goa.m_nNextCharArrayID[0];
-        pGameObjectArray->m_nNextCharArrayID[1] = goa.m_nNextCharArrayID[1];
+        pGameObjectArray->m_nNextObjectArrayID[0] = s_nNextObjectArrayID[0];
+        pGameObjectArray->m_nNextObjectArrayID[1] = s_nNextObjectArrayID[1];
+        pGameObjectArray->m_nNextCharArrayID[0]   = s_nNextCharArrayID[0];
+        pGameObjectArray->m_nNextCharArrayID[1]   = s_nNextCharArrayID[1];
     }
 }
 
@@ -384,93 +170,91 @@ static void SetupWrapperHooks()
 
 static CGameObject* GetGameObj(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    GetGameObject(nullptr, oid, &obj);
-    return obj;
+    return s_table.Get(oid);
 }
 static CNWSStore* GetStore(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Store ? static_cast<CNWSStore*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Store)
+        return static_cast<CNWSStore*>(obj);
+    return nullptr;
 }
 static CNWSItem* GetItem(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Item ? static_cast<CNWSItem*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Item)
+        return static_cast<CNWSItem*>(obj);
+    return nullptr;
 }
 static CNWSCreature* GetCreature(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Creature ? static_cast<CNWSCreature*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Creature)
+        return static_cast<CNWSCreature*>(obj);
+    return nullptr;
 }
 static CNWSModule* GetModule(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Module ? static_cast<CNWSModule*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Module)
+        return static_cast<CNWSModule*>(obj);
+    return nullptr;
 }
 static CNWSArea* GetArea(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Area ? static_cast<CNWSArea*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Area)
+        return static_cast<CNWSArea*>(obj);
+    return nullptr;
 }
 static CNWSTrigger* GetTrigger(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Trigger ? static_cast<CNWSTrigger*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Trigger)
+        return static_cast<CNWSTrigger*>(obj);
+    return nullptr;
 }
 static CNWSPlaceable* GetPlaceable(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Placeable ? static_cast<CNWSPlaceable*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Placeable)
+        return static_cast<CNWSPlaceable*>(obj);
+    return nullptr;
 }
 static CNWSDoor* GetDoor(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Door ? static_cast<CNWSDoor*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Door)
+        return static_cast<CNWSDoor*>(obj);
+    return nullptr;
 }
 static CNWSAreaOfEffectObject* GetAreaOfEffect(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::AreaOfEffect ? static_cast<CNWSAreaOfEffectObject*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::AreaOfEffect)
+        return static_cast<CNWSAreaOfEffectObject*>(obj);
+    return nullptr;
 }
 static CNWSWaypoint* GetWaypoint(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Waypoint ? static_cast<CNWSWaypoint*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Waypoint)
+        return static_cast<CNWSWaypoint*>(obj);
+    return nullptr;
 }
 static CNWSEncounter* GetEncounter(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Encounter ? static_cast<CNWSEncounter*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Encounter)
+        return static_cast<CNWSEncounter*>(obj);
+    return nullptr;
 }
 static CNWSSoundObject* GetSoundObject(CServerExoApp*, ObjectID oid)
 {
-    CGameObject* obj;
-    if (GetGameObject(nullptr, oid, &obj) != Success)
-        return nullptr;
-    return obj->m_nObjectType == Constants::ObjectType::Sound ? static_cast<CNWSSoundObject*>(obj) : nullptr;
+    auto* obj = s_table.Get(oid);
+    if (obj && obj->m_nObjectType == Constants::ObjectType::Sound)
+        return static_cast<CNWSSoundObject*>(obj);
+    return nullptr;
 }
 
 
