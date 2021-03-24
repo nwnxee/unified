0f9b10dbb9774578c602233242b9980b1b5bfa04
Aschent89 <tdnnwn@gmail.com>
2021-03-23 23:57:12 -0700

    Indent Fix

--- Plugins/Events/Events/AssociateEvents.cpp
+++ Plugins/Events/Events/AssociateEvents.cpp
@@ -5,89 +5,89 @@
 
 namespace Events {
 
-    using namespace NWNXLib;
-    using namespace NWNXLib::API;
+using namespace NWNXLib;
+using namespace NWNXLib::API;
 
-    static Hooks::Hook s_AddAssociateHook;
-    static Hooks::Hook s_RemoveAssociateHook;
-    static Hooks::Hook s_UnpossessFamiliarHook;
-    static Hooks::Hook s_PossessFamiliarHook;
+static Hooks::Hook s_AddAssociateHook;
+static Hooks::Hook s_RemoveAssociateHook;
+static Hooks::Hook s_UnpossessFamiliarHook;
+static Hooks::Hook s_PossessFamiliarHook;
 
-    AssociateEvents::AssociateEvents()
+AssociateEvents::AssociateEvents()
+{
+    Events::InitOnFirstSubscribe("NWNX_ON_ADD_ASSOCIATE_.*", []() {
+        s_AddAssociateHook = Hooks::HookFunction(API::Functions::_ZN12CNWSCreature12AddAssociateEjt,
+            (void*)&AddAssociateHook, Hooks::Order::Earliest);
+    });
+
+    Events::InitOnFirstSubscribe("NWNX_ON_REMOVE_ASSOCIATE_.*", []() {
+        s_RemoveAssociateHook = Hooks::HookFunction(API::Functions::_ZN12CNWSCreature15RemoveAssociateEj,
+            (void*)&RemoveAssociateHook, Hooks::Order::Earliest);
+    });
+
+    Events::InitOnFirstSubscribe("NWNX_ON_UNPOSSESS_FAMILIAR_.*", []() {
+        s_UnpossessFamiliarHook = Hooks::HookFunction(API::Functions::_ZN12CNWSCreature17UnpossessFamiliarEv,
+            (void*)&UnpossessFamiliarHook, Hooks::Order::Earliest);
+    });
+
+    Events::InitOnFirstSubscribe("NWNX_ON_POSSESS_FAMILIAR_.*", []() {
+        s_PossessFamiliarHook = Hooks::HookFunction(API::Functions::_ZN12CNWSCreature15PossessFamiliarEv,
+            (void*)&PossessFamiliarHook, Hooks::Order::Earliest);
+    });
+}
+
+void AssociateEvents::AddAssociateHook(CNWSCreature* thisPtr, ObjectID oidAssociate, uint16_t nAssociateType)
+{
+    Events::PushEventData("ASSOCIATE_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidAssociate));
+    Events::SignalEvent("NWNX_ON_ADD_ASSOCIATE_BEFORE", thisPtr->m_idSelf);
+    s_AddAssociateHook->CallOriginal<void>(thisPtr, oidAssociate, nAssociateType);
+    Events::PushEventData("ASSOCIATE_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidAssociate));
+    Events::SignalEvent("NWNX_ON_ADD_ASSOCIATE_AFTER", thisPtr->m_idSelf);
+}
+
+void AssociateEvents::RemoveAssociateHook(CNWSCreature* thisPtr, ObjectID oidAssociate)
+{
+    Events::PushEventData("ASSOCIATE_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidAssociate));
+    Events::SignalEvent("NWNX_ON_REMOVE_ASSOCIATE_BEFORE", thisPtr->m_idSelf);
+    s_RemoveAssociateHook->CallOriginal<void>(thisPtr, oidAssociate);
+    Events::PushEventData("ASSOCIATE_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidAssociate));
+    Events::SignalEvent("NWNX_ON_REMOVE_ASSOCIATE_AFTER", thisPtr->m_idSelf);
+}
+
+void AssociateEvents::UnpossessFamiliarHook(CNWSCreature* thisPtr)
+{
+    std::string sFamiliarOID = Utils::ObjectIDToString(thisPtr->GetAssociateId(Constants::AssociateType::Familiar));
+
+    auto PushAndSignalEvent = [&](const std::string& ev) -> bool {
+        Events::PushEventData("FAMILIAR", sFamiliarOID);
+        return Events::SignalEvent(ev, thisPtr->m_idSelf);
+    };
+
+    if (PushAndSignalEvent("NWNX_ON_UNPOSSESS_FAMILIAR_BEFORE"))
     {
-        Events::InitOnFirstSubscribe("NWNX_ON_ADD_ASSOCIATE_.*", []() {
-            s_AddAssociateHook = Hooks::HookFunction(API::Functions::_ZN12CNWSCreature12AddAssociateEjt,
-                (void*)&AddAssociateHook, Hooks::Order::Earliest);
-        });
-
-        Events::InitOnFirstSubscribe("NWNX_ON_REMOVE_ASSOCIATE_.*", []() {
-            s_RemoveAssociateHook = Hooks::HookFunction(API::Functions::_ZN12CNWSCreature15RemoveAssociateEj,
-                (void*)&RemoveAssociateHook, Hooks::Order::Earliest);
-        });
-
-        Events::InitOnFirstSubscribe("NWNX_ON_UNPOSSESS_FAMILIAR_.*", []() {
-            s_UnpossessFamiliarHook = Hooks::HookFunction(API::Functions::_ZN12CNWSCreature17UnpossessFamiliarEv,
-                (void*)&UnpossessFamiliarHook, Hooks::Order::Earliest);
-        });
-
-        Events::InitOnFirstSubscribe("NWNX_ON_POSSESS_FAMILIAR_.*", []() {
-            s_PossessFamiliarHook = Hooks::HookFunction(API::Functions::_ZN12CNWSCreature15PossessFamiliarEv,
-                (void*)&PossessFamiliarHook, Hooks::Order::Earliest);
-        });
+        s_UnpossessFamiliarHook->CallOriginal<void>(thisPtr);
     }
 
-    void AssociateEvents::AddAssociateHook(CNWSCreature* thisPtr, ObjectID oidAssociate, uint16_t nAssociateType)
+    PushAndSignalEvent("NWNX_ON_UNPOSSESS_FAMILIAR_AFTER");
+}
+
+
+
+void AssociateEvents::PossessFamiliarHook(CNWSCreature* thisPtr)
+{
+    std::string sFamiliarOID = Utils::ObjectIDToString(thisPtr->GetAssociateId(Constants::AssociateType::Familiar));
+
+    auto PushAndSignalEvent = [&](const std::string& ev) -> bool {
+        Events::PushEventData("FAMILIAR", sFamiliarOID);
+        return Events::SignalEvent(ev, thisPtr->m_idSelf);
+    };
+
+    if (PushAndSignalEvent("NWNX_ON_POSSESS_FAMILIAR_BEFORE"))
     {
-        Events::PushEventData("ASSOCIATE_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidAssociate));
-        Events::SignalEvent("NWNX_ON_ADD_ASSOCIATE_BEFORE", thisPtr->m_idSelf);
-        s_AddAssociateHook->CallOriginal<void>(thisPtr, oidAssociate, nAssociateType);
-        Events::PushEventData("ASSOCIATE_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidAssociate));
-        Events::SignalEvent("NWNX_ON_ADD_ASSOCIATE_AFTER", thisPtr->m_idSelf);
+        s_PossessFamiliarHook->CallOriginal<void>(thisPtr);
     }
 
-    void AssociateEvents::RemoveAssociateHook(CNWSCreature* thisPtr, ObjectID oidAssociate)
-    {
-        Events::PushEventData("ASSOCIATE_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidAssociate));
-        Events::SignalEvent("NWNX_ON_REMOVE_ASSOCIATE_BEFORE", thisPtr->m_idSelf);
-        s_RemoveAssociateHook->CallOriginal<void>(thisPtr, oidAssociate);
-        Events::PushEventData("ASSOCIATE_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidAssociate));
-        Events::SignalEvent("NWNX_ON_REMOVE_ASSOCIATE_AFTER", thisPtr->m_idSelf);
-    }
-
-    void AssociateEvents::UnpossessFamiliarHook(CNWSCreature* thisPtr)
-    {
-        std::string sFamiliarOID = Utils::ObjectIDToString(thisPtr->GetAssociateId(Constants::AssociateType::Familiar));
-
-        auto PushAndSignalEvent = [&](const std::string& ev) -> bool {
-            Events::PushEventData("FAMILIAR", sFamiliarOID);
-            return Events::SignalEvent(ev, thisPtr->m_idSelf);
-        };
-
-        if (PushAndSignalEvent("NWNX_ON_UNPOSSESS_FAMILIAR_BEFORE"))
-        {
-            s_UnpossessFamiliarHook->CallOriginal<void>(thisPtr);
-        }
-
-        PushAndSignalEvent("NWNX_ON_UNPOSSESS_FAMILIAR_AFTER");
-    }
-
-
-
-    void AssociateEvents::PossessFamiliarHook(CNWSCreature* thisPtr)
-    {
-        std::string sFamiliarOID = Utils::ObjectIDToString(thisPtr->GetAssociateId(Constants::AssociateType::Familiar));
-
-        auto PushAndSignalEvent = [&](const std::string& ev) -> bool {
-            Events::PushEventData("FAMILIAR", sFamiliarOID);
-            return Events::SignalEvent(ev, thisPtr->m_idSelf);
-        };
-
-        if (PushAndSignalEvent("NWNX_ON_POSSESS_FAMILIAR_BEFORE"))
-        {
-            s_PossessFamiliarHook->CallOriginal<void>(thisPtr);
-        }
-
-        PushAndSignalEvent("NWNX_ON_POSSESS_FAMILIAR_AFTER");
-    }
+    PushAndSignalEvent("NWNX_ON_POSSESS_FAMILIAR_AFTER");
+}
 
 }
