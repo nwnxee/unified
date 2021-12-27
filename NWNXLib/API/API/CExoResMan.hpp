4b4ca8a107d4ea083608f10eedfd0bb39506e5a7
Daz <31415508+Daztek@users.noreply.github.com>
2021-12-27 22:31:33 +0100

    API: update DataBlock definition

--- NWNXLib/API/API/CExoResMan.hpp
+++ NWNXLib/API/API/CExoResMan.hpp
@@ -10,8 +10,6 @@
 #include "CResRef.hpp"
 #include "RESID.hpp"
 
-#include <memory>
-
 #ifdef NWN_API_PROLOGUE
 NWN_API_PROLOGUE(CExoResMan)
 #endif
@@ -65,7 +63,7 @@ struct CExoResMan
     void DumpAllOfType(RESTYPE nType);
     void FreeResourceData(CRes * pRes);
     BOOL Exists(const CResRef & cResRef, RESTYPE nType, uint32_t * pTableType = nullptr);
-    std::shared_ptr<void*> Get(const CResRef & cResRef, RESTYPE nType);
+    DataBlockRef Get(const CResRef & cResRef, RESTYPE nType);
     CExoLocString GetEncapsulatedFileDescription(const CExoString & sFileName);
     CRes * GetResObject(const CResRef & cResRef, RESTYPE nType);
     CExoStringList * GetResOfType(RESTYPE nType, BOOL bERFOnly = false);
