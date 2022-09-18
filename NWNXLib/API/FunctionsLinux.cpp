cff8b4e4bedf79c43aabba01fb81dded86273787
niv <n@e-ix.net>
2022-09-18 15:20:19 +0200

    bring back FunctionsLinux as a extern/dlsym table so we don't have to rewrite 10 billion callsites

---
+++ NWNXLib/API/FunctionsLinux.cpp
@@ -0,0 +1,24 @@
+#include <dlfcn.h>
+#include <stdio.h>
+
+#include "Functions.hpp"
+
+#define NWNXLIB_FUNCTION_NO_VERSION_CHECK
+
+#define NWNXLIB_FUNCTION(name, address) \
+    void* NWNXLib::API::Functions::name;
+#include "FunctionsLinux.hpp"
+#undef NWNXLIB_FUNCTION
+
+void NWNXLib::API::Functions::Initialize()
+{
+#define NWNXLIB_FUNCTION(name, address)                    \
+    name = const_cast<void*>(dlsym(RTLD_DEFAULT, #name )); \
+    if (!name)                                             \
+    {                                                      \
+        printf("dlsym(%s) = null\n", #name);               \
+    }
+
+#include "FunctionsLinux.hpp"
+#undef NWNXLIB_FUNCTION
+}
