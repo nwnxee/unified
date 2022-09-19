3553178866e9ffe0c952392f9f18262ce7f94013
Jorteck <jorteckdev@gmail.com>
2022-09-19 19:38:23 +0200

    Experimental plugin function hooks.

--- Plugins/Experimental/UnhardcodeRangerDualWield.cpp
+++ Plugins/Experimental/UnhardcodeRangerDualWield.cpp
@@ -21,12 +21,11 @@ void UnhardcodeRangerDualWield()
 
     LOG_INFO("EXPERIMENTAL: Unhardcoding Ranger DualWield Feat.");
 
-    s_HasFeatHook = Hooks::HookFunction(API::Functions::_ZN17CNWSCreatureStats7HasFeatEt,
-        (void*)+[](CNWSCreatureStats* pCreatureStats, uint16_t nFeat) -> int32_t
+    s_HasFeatHook = Hooks::HookFunction(&CNWSCreatureStats::HasFeat,
+        +[](CNWSCreatureStats* pCreatureStats, uint16_t nFeat) -> int32_t
         {
             return pCreatureStats->m_lstFeats.Contains(nFeat) || pCreatureStats->m_lstBonusFeats.Contains(nFeat);
         }, Hooks::Order::Final);
 }
 
 }
-
