d373fac433d11c9bb86c7fe2d178ebf78caa2bc1
Daz <daztek@gmail.com>
2022-09-27 19:35:48 +0200

    API: quick'n'dirty update

--- NWNXLib/API/API/ObjectVisualTransformData.hpp
+++ NWNXLib/API/API/ObjectVisualTransformData.hpp
@@ -2,6 +2,7 @@
 #include "nwn_api.hpp"
 
 #include "CAurObjectVisualTransformData.hpp"
+#include <map>
 
 
 #ifdef NWN_API_PROLOGUE
@@ -12,10 +13,10 @@ NWN_API_PROLOGUE(ObjectVisualTransformData)
 
 
 
-struct ObjectVisualTransformData : CAurObjectVisualTransformData
+struct ObjectVisualTransformData
 {
-
-
+    std::map<int32_t, CAurObjectVisualTransformData> m_scopes;
+    ObjectVisualTransformData() {}
 
 #ifdef NWN_CLASS_EXTENSION_ObjectVisualTransformData
     NWN_CLASS_EXTENSION_ObjectVisualTransformData
