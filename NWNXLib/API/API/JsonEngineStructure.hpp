659472308e035442accabda185ffc0fe227f5b76
Daz <daztek@gmail.com>
2022-12-23 18:11:10 +0100

    Add back json support

--- NWNXLib/API/API/JsonEngineStructure.hpp
+++ NWNXLib/API/API/JsonEngineStructure.hpp
@@ -2,29 +2,47 @@
 #include "nwn_api.hpp"
 #include "CExoString.hpp"
 
-
 #ifdef NWN_API_PROLOGUE
 NWN_API_PROLOGUE(JsonEngineStructure)
 #endif
 
-
-struct JsonEngineStructure
+struct JsonEngineStructureShared
 {
+    uint64_t m_id = 0;
     json m_json;
     CExoString m_error;
+};
 
-    JsonEngineStructure() {}
-    JsonEngineStructure(const json& j, const CExoString& err = "") : m_json(j), m_error(err) {}
-    JsonEngineStructure(const JsonEngineStructure& other) : m_json(other.m_json), m_error(other.m_error) {}
-    JsonEngineStructure& operator=(const JsonEngineStructure& other)
+struct JsonEngineStructure : public SharedPtrEngineStructure<JsonEngineStructureShared>
+{
+    JsonEngineStructure()
     {
-        m_json = other.m_json;
-        m_error = other.m_error;
-        return *this;
+        m_shared = std::make_shared<JsonEngineStructureShared>();
+        m_shared->m_id = ++(*GetNextIdPtr());
     }
 
-    bool IsEmpty() const { return m_json.is_null(); }
-    void Clear() { m_json = {}; m_error = ""; }
+    JsonEngineStructure(const json& j, const CExoString& err)
+    {
+        m_shared = std::make_shared<JsonEngineStructureShared>();
+        m_shared->m_id = ++(*GetNextIdPtr());
+        m_shared->m_json = j;
+        m_shared->m_error = err;
+    }
+
+    JsonEngineStructure(json&& j, CExoString&& err)
+    {
+        m_shared = std::make_shared<JsonEngineStructureShared>();
+        m_shared->m_id = ++(*GetNextIdPtr());
+        m_shared->m_json = std::move(j);
+        m_shared->m_error = std::move(err);
+    }
+
+    virtual ~JsonEngineStructure() {}
+
+    static uint64_t* GetNextIdPtr();
+
+    bool IsEmpty() const { return m_shared->m_json.is_null(); }
+    void Clear() { m_shared->m_json = nullptr; m_shared->m_error = ""; }
 };
 
 
