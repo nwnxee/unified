145c1b4e61bbb6c1c9e0c6effe29edb995aaa3d2
Daz <daztek@gmail.com>
2023-01-14 22:44:13 +0100

    json: don't actually have to do all this weird stuff

--- NWNXLib/API/API/JsonEngineStructure.hpp
+++ NWNXLib/API/API/JsonEngineStructure.hpp
@@ -8,39 +8,17 @@ NWN_API_PROLOGUE(JsonEngineStructure)
 
 struct JsonEngineStructureShared
 {
-    uint64_t m_id = 0;
+    const uint64_t m_id;
     json m_json;
     CExoString m_error;
 };
 
 struct JsonEngineStructure : public SharedPtrEngineStructure<JsonEngineStructureShared>
 {
-    JsonEngineStructure()
-    {
-        m_shared = std::make_shared<JsonEngineStructureShared>();
-        m_shared->m_id = ++(*GetNextIdPtr());
-    }
-
-    JsonEngineStructure(const json& j, const CExoString& err)
-    {
-        m_shared = std::make_shared<JsonEngineStructureShared>();
-        m_shared->m_id = ++(*GetNextIdPtr());
-        m_shared->m_json = j;
-        m_shared->m_error = err;
-    }
-
-    JsonEngineStructure(json&& j, CExoString&& err)
-    {
-        m_shared = std::make_shared<JsonEngineStructureShared>();
-        m_shared->m_id = ++(*GetNextIdPtr());
-        m_shared->m_json = std::move(j);
-        m_shared->m_error = std::move(err);
-    }
-
+    JsonEngineStructure();
+    JsonEngineStructure(const json& j, const CExoString& err);
+    JsonEngineStructure(json&& j, CExoString&& err);
     virtual ~JsonEngineStructure() {}
-
-    static uint64_t* GetNextIdPtr();
-
     bool IsEmpty() const { return m_shared->m_json.is_null(); }
     void Clear() { m_shared->m_json = nullptr; m_shared->m_error = ""; }
 };
