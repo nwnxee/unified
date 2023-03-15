cc4db56c2b84e5b5aa54dfb9a667ffbc581c5fc0
Daz <daztek@gmail.com>
2023-03-15 21:54:26 +0100

    API: update for 87.8193.35-35

--- NWNXLib/API/API/CVirtualMachineStack.hpp
+++ NWNXLib/API/API/CVirtualMachineStack.hpp
@@ -35,9 +35,7 @@ struct CVirtualMachineStack
     BOOL SaveStack(CResGFF * pRes, CResStruct * pStruct);
     BOOL LoadStack(CResGFF * pRes, CResStruct * pStruct);
     void ClearStack();
-    int32_t GetBasePointer();
-    void SetBasePointer(int32_t nSize);
-    int32_t GetStackPointer();
+    inline int32_t GetStackPointer() { return m_nStackPointer; }
     void SetStackPointer(int32_t nSize);
 
 
