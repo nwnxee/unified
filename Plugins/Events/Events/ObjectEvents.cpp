a3f9fe21aa057468499ea031a8982025d5e3eb27
summonFox <46043493+summonFox@users.noreply.github.com>
2023-04-15 19:51:10 +1000

    Events: add `NWNX_ON_BROADCAST_SAFE_PROJECTILE_{BEFORE|AFTER}`

--- Plugins/Events/Events/ObjectEvents.cpp
+++ Plugins/Events/Events/ObjectEvents.cpp
@@ -10,10 +10,12 @@ using namespace NWNXLib::API::Constants;
 static NWNXLib::Hooks::Hook s_AddLockObjectActionHook;
 static NWNXLib::Hooks::Hook s_AddUnlockObjectActionHook;
 static NWNXLib::Hooks::Hook s_AddUseObjectActionHook;
+static NWNXLib::Hooks::Hook s_BroadcastSafeProjectileHook;
 
 static int32_t AddLockObjectActionHook(CNWSObject*, ObjectID);
 static int32_t AddUnlockObjectActionHook(CNWSObject*, ObjectID, ObjectID, int32_t);
 static int32_t AddUseObjectActionHook(CNWSObject*, ObjectID);
+static void BroadcastSafeProjectileHook(CNWSObject*, ObjectID, ObjectID, Vector, Vector, uint32_t, uint8_t, uint32_t, uint8_t, uint8_t);
 
 void ObjectEvents() __attribute__((constructor));
 void ObjectEvents()
@@ -31,6 +33,11 @@ void ObjectEvents()
     InitOnFirstSubscribe("NWNX_ON_OBJECT_USE_.*", []() {
         s_AddUseObjectActionHook = Hooks::HookFunction(&CNWSObject::AddUseObjectAction,
                                                    (void*)&AddUseObjectActionHook, Hooks::Order::Early);
+    });
+
+    InitOnFirstSubscribe("NWNX_ON_BROADCAST_SAFE_PROJECTILE_.*", []() {
+        s_BroadcastSafeProjectileHook = Hooks::HookFunction(&CNWSObject::BroadcastSafeProjectile,
+                                             (void*)&BroadcastSafeProjectileHook, Hooks::Order::Early);
     });
 }
 
@@ -108,6 +115,36 @@ int32_t AddUseObjectActionHook(CNWSObject *thisPtr, ObjectID oidObjectToUse)
     PushAndSignal("NWNX_ON_OBJECT_USE_AFTER");
 
     return retVal;
+}
+
+void BroadcastSafeProjectileHook(CNWSObject *thisPtr, ObjectID oidOriginator, ObjectID oidTarget, Vector vOriginator, Vector vTarget, uint32_t nDelta,
+                                    uint8_t nProjectileType, uint32_t nSpellID, uint8_t nAttackResult, uint8_t nProjectilePathType)
+{
+    if (!IsIDInWhitelist("NWNX_ON_BROADCAST_SAFE_PROJECTILE_TYPE", nProjectileType) || !IsIDInWhitelist("NWNX_ON_BROADCAST_SAFE_PROJECTILE_SPELL_ID", nSpellID))
+    {
+        s_BroadcastSafeProjectileHook->CallOriginal<void>(thisPtr, oidOriginator, oidTarget, vOriginator, vTarget, nDelta, nProjectileType, nSpellID, nAttackResult, nProjectilePathType);
+        return;
+    }
+
+    auto PushAndSignal = [&](const std::string& ev) -> bool {
+        PushEventData("TARGET_OBJECT_ID", Utils::ObjectIDToString(oidTarget));
+        PushEventData("TARGET_POSITION_X", std::to_string(vTarget.x));
+        PushEventData("TARGET_POSITION_Y", std::to_string(vTarget.y));
+        PushEventData("TARGET_POSITION_Z", std::to_string(vTarget.z));
+        PushEventData("DELTA", std::to_string(nDelta));
+        PushEventData("PROJECTILE_TYPE", std::to_string(nProjectileType));
+        PushEventData("SPELL_ID", std::to_string(nSpellID));
+        PushEventData("ATTACK_RESULT", std::to_string(nAttackResult));
+        PushEventData("PROJECTILE_PATH_TYPE", std::to_string(nProjectilePathType));
+        return SignalEvent(ev, thisPtr->m_idSelf);
+    };
+
+    if (PushAndSignal("NWNX_ON_BROADCAST_SAFE_PROJECTILE_BEFORE"))
+    {
+        s_BroadcastSafeProjectileHook->CallOriginal<void>(thisPtr, oidOriginator, oidTarget, vOriginator, vTarget, nDelta, nProjectileType, nSpellID, nAttackResult, nProjectilePathType);
+    }
+
+    PushAndSignal("NWNX_ON_BROADCAST_SAFE_PROJECTILE_AFTER");
 }
 
 }
