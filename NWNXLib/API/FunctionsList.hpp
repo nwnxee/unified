241cc329b261ff2a84d892091464b922854f44bb
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2023-07-19 23:35:49 +0200

    Add missing functions to FunctionsList.

--- NWNXLib/API/FunctionsList.hpp
+++ NWNXLib/API/FunctionsList.hpp
@@ -24,6 +24,7 @@ NWNXLIB_FUNCTION(_ZN22CNWSAreaOfEffectObject12EventHandlerEjjPvjj, ?EventHandler
 NWNXLIB_FUNCTION(_ZN8CNWSDoor12EventHandlerEjjPvjj, ?EventHandler@CNWSDoor@@UEAAXIIPEAXII@Z)
 NWNXLIB_FUNCTION(_ZN8CNWSItem12EventHandlerEjjPvjj, ?EventHandler@CNWSItem@@UEAAXIIPEAXII@Z)
 NWNXLIB_FUNCTION(_ZN9CNWSStore12EventHandlerEjjPvjj, ?EventHandler@CNWSStore@@UEAAXIIPEAXII@Z)
+
 NWNXLIB_FUNCTION(_ZN11CNWSTrigger8AIUpdateEv, ?AIUpdate@CNWSTrigger@@UEAAXXZ)
 NWNXLIB_FUNCTION(_ZN12CNWSCreature8AIUpdateEv, ?AIUpdate@CNWSCreature@@UEAAXXZ)
 NWNXLIB_FUNCTION(_ZN12CNWSWaypoint8AIUpdateEv, ?AIUpdate@CNWSWaypoint@@UEAAXXZ)
@@ -35,8 +36,15 @@ NWNXLIB_FUNCTION(_ZN22CNWSAreaOfEffectObject8AIUpdateEv, ?AIUpdate@CNWSAreaOfEff
 NWNXLIB_FUNCTION(_ZN8CNWSDoor8AIUpdateEv, ?AIUpdate@CNWSDoor@@UEAAXXZ)
 NWNXLIB_FUNCTION(_ZN8CNWSItem8AIUpdateEv, ?AIUpdate@CNWSItem@@UEAAXXZ)
 NWNXLIB_FUNCTION(_ZN9CNWSStore8AIUpdateEv, ?AIUpdate@CNWSStore@@UEAAXXZ)
+NWNXLIB_FUNCTION(_ZN12CNWSCreature11ClearActionEP20CNWSObjectActionNodei, ?ClearAction@CNWSCreature@@MEAAHPEAVCNWSObjectActionNode@@H@Z);
+
 NWNXLIB_FUNCTION(_ZN21CNWSEffectListHandler15OnEffectAppliedEP10CNWSObjectP11CGameEffecti, ?OnEffectApplied@CNWSEffectListHandler@@UEAAHPEAVCNWSObject@@PEAVCGameEffect@@H@Z)
 NWNXLIB_FUNCTION(_ZN21CNWSEffectListHandler15OnEffectRemovedEP10CNWSObjectP11CGameEffect, ?OnEffectRemoved@CNWSEffectListHandler@@UEAAHPEAVCNWSObject@@PEAVCGameEffect@@@Z)
+
+NWNXLIB_FUNCTION(_ZN16CNWSAmbientSound15PlayBattleMusicEi, ?PlayBattleMusic@CNWSAmbientSound@@UEAAXH@Z);
+
+NWNXLIB_FUNCTION(_ZN10CNWSObject19GetCurrentHitPointsEi, ?GetCurrentHitPoints@CNWSObject@@UEAAFH@Z);
+NWNXLIB_FUNCTION(_ZN12CNWSCreature15GetMaxHitPointsEi, ?GetMaxHitPoints@CNWSCreature@@UEAAFH@Z);
 
 // All other hooked functions - needed for Windows as it has scary pointers for member functions.
 
