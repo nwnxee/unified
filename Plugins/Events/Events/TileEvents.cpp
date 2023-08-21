4568a1d977908752f0d731a34876b83172ce7653
Daz <daztek@gmail.com>
2023-08-21 21:47:13 +0200

    Refactor and simplify `NWNX_ON_MATERIALCHANGE_*`

--- Plugins/Events/Events/TileEvents.cpp
+++ Plugins/Events/Events/TileEvents.cpp
@@ -3,52 +3,60 @@
 #include "API/CServerExoApp.hpp"
 #include "API/CNWSArea.hpp"
 #include "API/CNWSObject.hpp"
+#include "API/CNWSCreature.hpp"
 
 namespace Events {
 
 using namespace NWNXLib;
 
-static std::unordered_map<ObjectID, int32_t> m_objectCurrentMaterial;
-static Hooks::Hook s_SetPositionHook;
+static Hooks::Hook s_SetPositionMaterialChangeHook;
 
-static void SetPositionHook(CNWSObject*, Vector, int32_t);
+static void SetPositionMaterialChangeHook(CNWSObject*, Vector, int32_t);
 
 void TileEvents() __attribute__((constructor));
 void TileEvents()
 {
     InitOnFirstSubscribe("NWNX_ON_MATERIALCHANGE_.*", []() {
-        s_SetPositionHook = Hooks::HookFunction(&CNWSObject::SetPosition,
-                                         &SetPositionHook, Hooks::Order::Earliest);
+        s_SetPositionMaterialChangeHook = Hooks::HookFunction(&CNWSObject::SetPosition,
+            &SetPositionMaterialChangeHook, Hooks::Order::Earliest);
     });
 }
 
-void SetPositionHook(CNWSObject* thisPtr, Vector vPosition, BOOL bDoingCharacterCopy)
+void SetPositionMaterialChangeHook(CNWSObject* thisPtr, Vector vPosition, BOOL bDoingCharacterCopy)
 {
     if (thisPtr->m_nObjectType != API::Constants::ObjectType::Creature)
     {
-        s_SetPositionHook->CallOriginal<void>(thisPtr, vPosition, bDoingCharacterCopy);
+        s_SetPositionMaterialChangeHook->CallOriginal<void>(thisPtr, vPosition, bDoingCharacterCopy);
         return;
     }
 
-    if (auto *pArea = API::Globals::AppManager()->m_pServerExoApp->GetAreaByGameObjectID(thisPtr->m_oidArea))
+    if (auto *pCreature = Utils::AsNWSCreature(thisPtr))
     {
-        auto iMat = pArea->GetSurfaceMaterial(vPosition);
-        if (!m_objectCurrentMaterial.count(thisPtr->m_idSelf) || iMat != m_objectCurrentMaterial[thisPtr->m_idSelf])
+        CNWSArea *pArea = API::Globals::AppManager()->m_pServerExoApp->GetAreaByGameObjectID(pCreature->m_oidArea);
+        if (!pArea)
+            pArea = API::Globals::AppManager()->m_pServerExoApp->GetAreaByGameObjectID(pCreature->m_oidDesiredArea);
+
+        if (pArea)
         {
-            PushEventData("MATERIAL_TYPE", std::to_string(iMat));
-            SignalEvent("NWNX_ON_MATERIALCHANGE_BEFORE", thisPtr->m_idSelf);
+            int32_t oldMaterial = pArea->GetSurfaceMaterial(pCreature->m_vPosition);
+            int32_t newMaterial = pArea->GetSurfaceMaterial(vPosition);
 
-            s_SetPositionHook->CallOriginal<void>(thisPtr, vPosition, bDoingCharacterCopy);
+            if (oldMaterial != newMaterial || pCreature->m_vPosition == vPosition)
+            {
+                PushEventData("MATERIAL_TYPE", std::to_string(newMaterial));
+                SignalEvent("NWNX_ON_MATERIALCHANGE_BEFORE", thisPtr->m_idSelf);
 
-            PushEventData("MATERIAL_TYPE", std::to_string(iMat));
-            SignalEvent("NWNX_ON_MATERIALCHANGE_AFTER", thisPtr->m_idSelf);
+                s_SetPositionMaterialChangeHook->CallOriginal<void>(thisPtr, vPosition, bDoingCharacterCopy);
 
-            m_objectCurrentMaterial[thisPtr->m_idSelf] = iMat;
-            return;
+                PushEventData("MATERIAL_TYPE", std::to_string(newMaterial));
+                SignalEvent("NWNX_ON_MATERIALCHANGE_AFTER", thisPtr->m_idSelf);
+
+                return;
+            }
         }
     }
 
-    s_SetPositionHook->CallOriginal<void>(thisPtr, vPosition, bDoingCharacterCopy);
+    s_SetPositionMaterialChangeHook->CallOriginal<void>(thisPtr, vPosition, bDoingCharacterCopy);
 }
 
 }
